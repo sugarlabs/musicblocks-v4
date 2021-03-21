8dc4e267446929462a320fd5c31c255f11e4ce42
Anindya Kundu <anindya.k22@outlook.com>
2021-03-22 02:47:14 +0530

    refactor: reword the tests
    Reword the unit test section names and the unit test names to be more specific.

--- src/.prototype/musicutils/ts/temperament.test.ts
+++ src/.prototype/musicutils/ts/temperament.test.ts
@@ -1,62 +1,63 @@
-/*
-    Unit tests for musicutils
-*/
+import Temperament from './temperament';
 
-// Copyright 2021 Walter Bender, Sugar Labs
+describe('class Temperament', () => {
+    describe('Temperament generation', () => {
+        test("Generate 'equal 12' temperament instance and verify members", () => {
+            const t = new Temperament();
+            expect(t.name).toBe('equal');
 
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the The GNU Affero General Public
-// License as published by the Free Software Foundation; either
-// version 3 of the License, or (at your option) any later version.
+            t.baseFrequency = Temperament.C0;
+            expect(t.baseFrequency).toBe(Temperament.C0);
+            expect(t.numberOfOctaves).toBe(8);
 
-// You should have received a copy of the GNU Affero General Public
-// License along with this library; if not, write to the Free Software
-// Foundation, 51 Franklin Street, Suite 500 Boston, MA 02110-1335 USA
+            const f: number[] = t.freqs;
+            expect(Number(f[21].toFixed(2))).toBe(55.0);
+            expect(t.noteNames.length).toBe(12);
+        });
 
-import { Temperament } from './temperament';
+        test("Generate 'pythagorean' temperament instance and verify frequencies", () => {
+            const t = new Temperament('pythagorean');
+            const f: number[] = t.freqs;
+            expect(Number(f[21].toFixed(2))).toBe(55.19); // A1
+            expect(t.noteNames.length).toBe(12);
+        });
 
-describe('Temperament Tests', () => {
-    test('equal 12', () => {
+        test("Generate 'just intonation' temperament instance and verify frequencies", () => {
+            const t = new Temperament('just intonation');
+            const f: number[] = t.freqs;
+            expect(Number(f[21].toFixed(2))).toBe(54.51); // A1
+            expect(t.noteNames.length).toBe(12);
+        });
+
+        test("Generate 'quarter comma meantone' temperament instance and verify frequencies", () => {
+            const t = new Temperament('quarter comma meantone');
+            const f: number[] = t.freqs;
+            expect(Number(f[36].toFixed(2))).toBe(55.45); // A1
+            expect(t.noteNames.length).toBe(21);
+        });
+
+        test("Generate 'equal 24' temperament instance and verify frequencies", () => {
+            const t = new Temperament();
+            t.generateEqualTemperament(24);
+            const f: number[] = t.freqs;
+            expect(Number(f[42].toFixed(2))).toBe(55.0); // A1
+            expect(t.noteNames.length).toBe(24);
+        });
+    });
+
+    describe('Tuning', () => {
         const t = new Temperament();
-        expect(t.name).toBe('equal');
-        t.baseFrequency = t.C0;
-        expect(t.baseFrequency).toBe(t.C0);
-        expect(t.numberOfOctaves).toBe(8);
-        const f: Array<number> = t.freqs;
-        expect(Number(f[21].toFixed(2))).toBe(55.0);
-        expect(t.noteNames.length).toBe(12);
-    });
-    test('pythagorean', () => {
-        const t = new Temperament('pythagorean');
-        const f: Array<number> = t.freqs;
-        expect(Number(f[21].toFixed(2))).toBe(55.19); // A1
-        expect(t.noteNames.length).toBe(12);
-    });
-    test('just intonation', () => {
-        const t = new Temperament('just intonation');
-        const f: Array<number> = t.freqs;
-        expect(Number(f[21].toFixed(2))).toBe(54.51); // A1
-        expect(t.noteNames.length).toBe(12);
-    });
-    test('quarter comma meantone', () => {
-        const t = new Temperament('quarter comma meantone');
-        const f: Array<number> = t.freqs;
-        expect(Number(f[36].toFixed(2))).toBe(55.45); // A1
-        expect(t.noteNames.length).toBe(21);
-    });
-    test('equal 24', () => {
-        const t = new Temperament();
-        t.generateEqualTemperament(24);
-        const f: Array<number> = t.freqs;
-        expect(Number(f[42].toFixed(2))).toBe(55.0); // A1
-        expect(t.noteNames.length).toBe(24);
-    });
-    test('test tuning', () => {
-        const t = new Temperament();
-        t.tune('a', 4, 440.0);
-        expect(Number(t.baseFrequency.toFixed(2))).toBe(16.35);
-        t.tune('a', 4, 441.0);
-        expect(Number(t.baseFrequency.toFixed(2))).toBe(16.39);
-        expect(Number(t.freqs[57].toFixed(2))).toBe(441.0);
+
+        test('Tune A4 to 440 Hz and expect 58th frequency to be 440Hz C0 (1st) to be 16.35 Hz', () => {
+            t.tune('a', 4, 440.0);
+            expect(Number(t.freqs[57].toFixed(2))).toBe(440.0);
+            expect(Number(t.baseFrequency.toFixed(2))).toBe(16.35);
+        });
+
+        test('Tune A4 to 441 Hz and expect C0 to be 16.39 Hz', () => {
+            t.tune('a', 4, 441.0);
+            expect(Number(t.baseFrequency.toFixed(2))).toBe(16.39);
+            expect(Number(t.freqs[57].toFixed(2))).toBe(441.0);
+        });
     });
 });

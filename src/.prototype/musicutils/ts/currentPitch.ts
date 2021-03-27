edcf80f85250a09704d5f4ba258537c686a08032
Anindya Kundu <anindya.k22@outlook.com>
2021-03-28 02:14:08 +0530

    refactor: improve docs, minor code retouching
    Improve on the documentation (plus cosmetic changes) extensively. Add minor code improvements and corrections (related to types) sparingly.

--- src/.prototype/musicutils/ts/currentPitch.ts
+++ src/.prototype/musicutils/ts/currentPitch.ts
@@ -1,65 +1,59 @@
 /*
-The Current Pitch class defines an object that manages pitch state.
-*/
+ * Copyright (c) 2021, Walter Bender. All rights reserved.
+ * Copyright (c) 2021, Kumar Saurabh Raj. All rights reserved.
+ * Copyright (c) 2021, Anindya Kundu. All rights reserved.
+ *
+ * Licensed under the AGPL-3.0 License.
+ */
 
-// Copyright (c) 2021 Walter Bender, Sugar Labs
-
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the The GNU Affero General Public
-// License as published by the Free Software Foundation; either
-// version 3 of the License, or (at your option) any later version.
-
-// You should have received a copy of the GNU Affero General Public
-// License along with this library; if not, write to the Free Software
-// Foundation, 51 Franklin Street, Suite 500 Boston, MA 02110-1335 USA
-
+import { ICurrentPitch } from './@types/currentPitch';
 import Temperament from './temperament';
 import KeySignature from './keySignature';
-import ICurrentPitch from './@types/currentPitch';
 
-// from temperament import Temperament
-// from keysignature import KeySignature
-
+/**
+ * Defines an object that manages pitch state.
+ *
+ * @remarks
+ * A pitch is a note within a scale and temperament (tuning system).
+ */
 export default class CurrentPitch implements ICurrentPitch {
-    /*
-    A pitch is a note within a scale and temperament (tuning system).
-    */
-
-    private _freq: number;
-    private _number: number;
+    /** The associated `Temperament` instance. */
     private _t: Temperament;
-    private _octave: number;
+    /** The associated `KeySignature` instance. */
     private _ks: KeySignature;
+    /** The frequency of the current note in Hertz. */
+    private _freq: number;
+    /**
+     * The index of the current note within the list of all of the notes defined by the temperament.
+     */
+    private _number: number;
+    /** The octave of the current note. */
+    private _octave: number;
+    /** The generic name of the current note. */
     private _genericName: string;
+    /** The modal index of the current note. */
     private _semitoneIndex: number;
 
     /**
-     * We need to define a key signature and temperament and a starting
-        point within the scale (and an initial octave value.
-     * @param keysignature - KeySignature object; defaults to KeySignature(key="c", mode="major")
-     * @param temperament - Temperament object; defaults to Temperament(name="equal")
-     * @param i - Index into semitones defined by temperament; defaults to 7, which
-            maps to g (sol) in an equal temperament tuning
-     * @param octave - Initial octave for the pitch. Defaults to Octave 4
+     * We need to define a key signature and temperament and a starting point within the scale (and
+     * an initial octave value.
+     *
+     * @param keySignature - `KeySignature` object; defaults to KeySignature(key="c", mode="major").
+     * @param temperament - `Temperament` object; defaults to Temperament(name="equal").
+     * @param i - Index into semitones defined by temperament; defaults to `7`, which maps to g
+     * (sol) in an equal temperament tuning.
+     * @param octave - Initial octave for the pitch. Defaults to Octave 4.
      */
     constructor(
-        keysignature: any = null,
+        keySignature: KeySignature = null,
         temperament: Temperament = null,
         i: number = 7,
         octave: number = 4
     ) {
-        if (!temperament) {
-            this._t = new Temperament();
-        } else {
-            this._t = temperament;
-        }
-
-        if (!keysignature) {
-            this._ks = new KeySignature('major', 'c', this._t.numberOfSemitonesInOctave);
-        } else {
-            this._ks = keysignature;
-        }
-
+        this._t = !temperament ? new Temperament() : temperament;
+        this._ks = !keySignature
+            ? new KeySignature('major', 'c', this._t.numberOfSemitonesInOctave)
+            : keySignature;
         this._semitoneIndex = i;
         this._octave = octave;
 
@@ -70,57 +64,101 @@ export default class CurrentPitch implements ICurrentPitch {
         this._number = this._octave * this._t.numberOfSemitonesInOctave + this._semitoneIndex;
     }
 
-    private _defineByFrequency(freq: any): void {
+    /**
+     * The frequency of the current note in Hertz.
+     */
+    public get freq(): number {
+        return this._freq;
+    }
+
+    /**
+     * The octave of the current note.
+     */
+    public get octave(): number {
+        return this._octave;
+    }
+
+    /**
+     * The generic name of the current note.
+     */
+    public get genericName(): string {
+        return this._genericName;
+    }
+
+    /**
+     * The modal index of the current note.
+     */
+    public get semitoneIndex(): number {
+        return this._semitoneIndex;
+    }
+
+    /**
+     * The index of the current note within the list of all of the notes defined by the temperament.
+     */
+    public get number(): number {
+        return this._number;
+    }
+
+    /**
+     * Defines the current pitch using a frequency.
+     *
+     * @param freq - The frequency used to define the current pitch.
+     */
+    private _defineByFrequency(freq: number): void {
         // Assume it is a frequency, and ignore the octave.
         this._number = this._t.getNearestFreqIndex(freq);
         // Do we force the pitch to be in the temperament?
         this._freq = this._t.getFreqByIndex(this._number);
-        const [_semitoneIndex, _octave] = this._t.getModalIndexAndOctaveFromFreqIndex(this._number);
-        this._semitoneIndex = _semitoneIndex;
-        this._octave = _octave;
+        [this._semitoneIndex, this._octave] = this._t.getModalIndexAndOctaveFromFreqIndex(
+            this._number
+        );
         this._genericName = this._t.getNoteName(this._semitoneIndex);
     }
 
     /**
-     * Set current pitch to a new pitch by frequency, index and octave or
-        name. These internal states are updates: freq, semitoneIndex,
-        genericName, octave, and number.
-     * @param pitchName - The new pitch as a frequency (float), or a modal index (int) and
-            octave or note name (str) and octave.
-            Note names can be "n7", "g", "sol", or, if defined, a custom name.
-     * @param octave - The new octave (not needed when pitch is specified by frequency)
+     * Sets current pitch to a new pitch by frequency, index and octave or name. These internal
+     * states are updates: freq, semitoneIndex, genericName, octave, and number.
+     *
+     * @param pitchName - The new pitch as a frequency (float), or a modal index (int) and octave or
+     * note name (str) and octave. Note names can be "n7", "g", "sol", or, if defined, a custom name.
+     * @param octave - The new octave (not needed when pitch is specified by frequency).
      */
-    setPitch(pitchName: number | string, octave: number = 4): void {
+    public setPitch(pitchName: number | string, octave: number = 4): void {
         if (typeof pitchName === 'number') {
-            this._defineByFrequency(pitchName);
-        } else if (typeof pitchName === 'number') {
-            // A few assumptions here: If the int > number of defined
-            // in the temperament, assume it is a frequency.  If the
-            // int < number of semitones in an octave, assume it is a
-            // semitone index.  Otherwise, assume it is an index in the
-            // list of frequencies (a pitch number).
-            if (pitchName > this._t.numberOfNotesInTemperament) {
+            // pitchName is float
+            if (pitchName % 1 !== 0) {
                 this._defineByFrequency(pitchName);
-            } else if (pitchName > this._t.numberOfSemitonesInOctave) {
-                // Assume it is a pitch number
-                this._number = pitchName;
-                this._freq = this._t.getFreqByIndex(this._number);
-                const [_semitoneIndex, _octave] = this._t.getModalIndexAndOctaveFromFreqIndex(
-                    this._number
-                );
-                this._semitoneIndex = _semitoneIndex;
-                this._octave = _octave;
-                this._genericName = this._t.getNoteName(this._semitoneIndex);
-            } else {
-                // Assume it is a semitone index.
-                this._semitoneIndex = pitchName;
-                this._octave = octave;
-                this._genericName = this._t.getNoteName(this._semitoneIndex);
-                this._freq = this._t.getFreqByModalIndexAndOctave(
-                    this._semitoneIndex,
-                    this._octave
-                );
-                this._number = this._t.getNearestFreqIndex(this._freq);
+            }
+            // pitchName is int
+            else {
+                /*
+                 * A few assumptions here: If the int > number of defined in the temperament, assume it
+                 * is a frequency. If the int < number of semitones in an octave, assume it is a
+                 * semitone index. Otherwise, assume it is an index in the list of frequencies (a pitch
+                 * number).
+                 */
+                if (pitchName > this._t.numberOfNotesInTemperament) {
+                    this._defineByFrequency(pitchName);
+                } else if (pitchName > this._t.numberOfSemitonesInOctave) {
+                    // Assume it is a pitch number.
+                    this._number = pitchName;
+                    this._freq = this._t.getFreqByIndex(this._number);
+                    [
+                        this._semitoneIndex,
+                        this._octave
+                    ] = this._t.getModalIndexAndOctaveFromFreqIndex(this._number);
+                    this._genericName = this._t.getNoteName(this._semitoneIndex);
+                } else {
+                    // Assume it is a semitone index.
+                    this._semitoneIndex = pitchName;
+                    this._octave = octave;
+                    this._genericName = this._t.getNoteName(this._semitoneIndex);
+                    this._freq = this._t.getFreqByModalIndexAndOctave(
+                        this._semitoneIndex,
+                        this._octave
+                    );
+                    this._number = this._t.getNearestFreqIndex(this._freq);
+                }
             }
         } else if (typeof pitchName === 'string') {
             // Assume it is a name of some sort.
@@ -133,15 +171,15 @@ export default class CurrentPitch implements ICurrentPitch {
     }
 
     /**
-     * Update the current note by applying a semitone transposition.
-        These internal states are updates: freq, semitoneIndex,
-        genericName, octave, and number.
-     * @param numberOfHalfSteps - The transposition in half steps
+     * Updates the current note by applying a semitone transposition. These internal states are
+     * updates: freq, semitoneIndex, genericName, octave, and number.
+     *
+     * @param numberOfHalfSteps - The transposition in half steps.
      */
-    applySemitoneTransposition(numberOfHalfSteps: number): void {
+    public applySemitoneTransposition(numberOfHalfSteps: number): void {
         const [_genericName, deltaOctave] = this._ks
             .semitoneTransform(this._genericName, numberOfHalfSteps)
-            .slice(0, 2);
+            .slice(0, 2) as [string, number];
         this._genericName = _genericName;
         this._octave += deltaOctave;
         this._semitoneIndex = this._t.getModalIndex(this._genericName);
@@ -150,86 +188,49 @@ export default class CurrentPitch implements ICurrentPitch {
     }
 
     /**
-     * Update the current note by applying a scalar transposition.
-        These internal states are updates: freq, semitoneIndex,
-        genericName, octave, and number.
-     * @param numberOfScalarSteps - The transposition in scalar steps
+     * Updates the current note by applying a scalar transposition. These internal states are
+     * updates: freq, semitoneIndex, genericName, octave, and number.
+     *
+     * @param numberOfScalarSteps - The transposition in scalar steps.
      */
-    applyScalarTransposition(numberOfScalarSteps: number): void {
+    public applyScalarTransposition(numberOfScalarSteps: number): void {
         const [_genericName, deltaOctave] = this._ks
             .scalarTransform(this._genericName, numberOfScalarSteps)
-            .slice(0, 2);
-
-        this._genericName = _genericName as string;
-        this._octave += deltaOctave as number;
+            .slice(0, 2) as [string, number];
+        this._genericName = _genericName;
+        this._octave += deltaOctave;
         this._semitoneIndex = this._t.getModalIndex(this._genericName);
         this._freq = this._t.getFreqByModalIndexAndOctave(this._semitoneIndex, this._octave);
         this._number = this._t.getNearestFreqIndex(this._freq);
     }
 
     /**
-     * Calculate the frequency of the note numberOfHalfSteps
-        away from the current note.
-     * @param numberOfHalfSteps - The interval in half steps
-     * @returns - The frequency of the note at the specified interval.
+     * Calculates the frequency of the note numberOfHalfSteps away from the current note.
+     *
+     * @param numberOfHalfSteps - The interval in half steps.
+     * @returns The frequency of the note at the specified interval.
      */
-    getSemitoneInterval(numberOfHalfSteps: number): number {
+    public getSemitoneInterval(numberOfHalfSteps: number): number {
         const [genericName, deltaOctave] = this._ks
             .semitoneTransform(this._genericName, numberOfHalfSteps)
-            .slice(0, 2);
+            .slice(0, 2) as [string, number];
         const semitoneIndex = this._t.getModalIndex(genericName);
         const octave = this._octave + deltaOctave;
         return this._t.getFreqByModalIndexAndOctave(semitoneIndex, octave);
     }
 
     /**
-     * Calculate the frequency of the note numberOfScalarSteps
-        away from the current note.
-     * @param numberOfScalarSteps - The interval in scalar steps
-     * @returns - The frequency of the note at the specified interval.
+     * Calculates the frequency of the note numberOfScalarSteps away from the current note.
+     *
+     * @param numberOfScalarSteps - The interval in scalar steps.
+     * @returns The frequency of the note at the specified interval.
      */
-    getScalarInterval(numberOfScalarSteps: number): number {
+    public getScalarInterval(numberOfScalarSteps: number): number {
         const [genericName, deltaOctave] = this._ks
             .scalarTransform(this._genericName, numberOfScalarSteps)
-            .slice(0, 2);
-        const semitoneIndex = this._t.getModalIndex(genericName as string);
-        const octave = this._octave + (deltaOctave as number);
+            .slice(0, 2) as [string, number];
+        const semitoneIndex = this._t.getModalIndex(genericName);
+        const octave = this._octave + deltaOctave;
         return this._t.getFreqByModalIndexAndOctave(semitoneIndex, octave);
-    }
-
-    /**
-     * @returns - The frequency of the current note in Hertz
-     */
-    get freq(): number {
-        return this._freq;
-    }
-
-    /**
-     * @returns - The octave of the current note
-     */
-    get octave(): number {
-        return this._octave;
-    }
-
-    /**
-     * @returns - The generic name of the current note
-     */
-    get genericName(): string {
-        return this._genericName;
-    }
-
-    /**
-     * @returns - The modal index of the current note
-     */
-    get semitoneIndex(): number {
-        return this._semitoneIndex;
-    }
-
-    /**
-     * @returns - The index of the current note within the list of all of the
-            notes defined by the temperament
-     */
-    get number(): number {
-        return this._number;
     }
 }

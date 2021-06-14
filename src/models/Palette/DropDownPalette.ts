5843088302fc89d236fd70b431960899d8668a8a
joykirat18 <joykiratsingh18@gmail.com>
2021-06-15 02:48:51 +0530

    modified models

---
+++ src/models/Palette/DropDownPalette.ts
@@ -0,0 +1,11 @@
+export default class DropDownPalette {
+    // List of advance category (fetched from engine)
+
+    // Block corresponding to advance category(fetched from engine)
+
+    selectedCategory: string | null;
+
+    constructor() {
+        this.selectedCategory = null;
+    }
+}

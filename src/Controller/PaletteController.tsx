a2c6a2993021e410e1ef0572c82b830ca7c77407
joykirat18 <joykiratsingh18@gmail.com>
2021-06-15 02:31:48 +0530

    Controller for Palette

---
+++ src/Controller/PaletteController.tsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import { blockList, buttonList, category } from "../dummyEngine";
+import BlockView from "../view/Palette/BlockView";
+import PaletteView from "../view/Palette/PaletteView";
+import SubCategoryView from "../view/Palette/SubCategoryView";
+
+const PaletteContoller: React.FC = () => {
+  const [showMenu, SetShowMenu] = useState(false);
+  const [selectedCategory, SetSelectedCategory] = useState<string>("");
+  const [showBlocks, SetShowBlocks] = useState(false);
+  const [selectedSubCategory, SetSelectedSubCategory] = useState<string>("");
+
+  const onClickCategory = (text: string) => {
+    SetShowMenu(true);
+    SetShowBlocks(false);
+    SetSelectedCategory(text);
+  };
+
+  const removeSubCategory = () => {
+    SetShowMenu(false);
+  };
+
+  const onClickSubCategory = (text: string) => {
+    SetShowBlocks(true);
+    SetSelectedSubCategory(text);
+    // console.log(blockList[text]);
+  };
+
+  const removeBlocks = () => {
+    SetShowBlocks(false);
+  };
+
+  return (
+    <div>
+      <PaletteView
+        openSubCategories={onClickCategory}
+        showMenu={showMenu}
+        removeSubCategory={removeSubCategory}
+      />
+
+      {showMenu ? (
+        <SubCategoryView
+          buttonList={buttonList[category.indexOf(selectedCategory)]}
+          openblocks={onClickSubCategory}
+        />
+      ) : null}
+
+      {showMenu && showBlocks ? (
+        <BlockView
+          blockList={blockList[selectedSubCategory]}
+          showBlocks={showBlocks}
+          removeBlocks={removeBlocks}
+        />
+      ) : null}
+    </div>
+  );
+};
+
+export default PaletteContoller;

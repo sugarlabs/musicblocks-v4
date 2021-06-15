943eccddaf078d55fcfa71fcfd7d8fba6d21d470
Daksh Doshi <ddaksh.2711@gmail.com>
2021-06-15 13:40:09 +0530

    update model

--- src/models/menuModel.ts
+++ src/models/menuModel.ts
@@ -3,18 +3,14 @@ class MenuModel {
     // the language opted by the user
     language: string;
 
-    // determines if the mode is beginner or advanced
-    beginnerMode: boolean;
-
     // determines if the feature to save mouse artwork as SVG is enabled or not
     saveSVG: boolean;
 
     // determines if the feature to save mouse artwork as PNG is enabled or not
     savePNG: boolean;
 
-    constructor(language: string, mode: boolean) {
+    constructor(language: string) {
         this.language = language;
-        this.beginnerMode = mode;
         this.saveSVG = false;
         this.savePNG = false;
     }

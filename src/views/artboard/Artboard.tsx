51714c23f50274f8a3aac9a06a673e1774908c86
b18050 <abchandan11@gmail.com>
2021-07-21 12:30:41 +0530

    feat: called sketch for artboard and turtle with some props
    (cherry picked from commit b3466f6c498c6e35de7aec151c36bf95c61101c2)

--- src/views/artboard/Artboard.tsx
+++ src/views/artboard/Artboard.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 // -- types ----------------------------------------------------------------------------------------
 
 import { IArtboardProps } from '../../@types/artboard';
@@ -8,24 +8,70 @@ import { IArtboardProps } from '../../@types/artboard';
 import './Artboard.scss';
 
 // -- view component definition --------------------------------------------------------------------
-import ArtboardSketch from './ArtboardSketch';
-import ArtboardTurtle from './ArtboardTurtle';
+import { ArtboardSketch, boardSketch } from './ArtboardSketch';
+import { ArtboardTurtleSketch, turtleSketch } from './ArtboardTurtle';
 /**
  * View of the Artboard Framework component.
  *
  * @returns root JSX element
  */
 export default function (props: IArtboardProps): JSX.Element {
+  const [id, setId] = useState<number>(0);
+  const [doMove, setDoMove] = useState<boolean>();
+  const [doRotate, setDoRotation] = useState<boolean>();
+  const [doMakeArc, setDoMakeArc] = useState<boolean>();
+  const [turtleColor, setTurtleColor] = useState<string>();
+  const colors = ['red', 'magenta', 'pink', 'orange', 'green', 'blue'];
+
+  const getRandomColor = () => {
+    return colors[Math.floor(Math.random() * colors.length)];
+  };
+  const handleMove = () => setDoMove(!doMove);
+  const handleRotate = () => setDoRotation(!doRotate);
+  const handleArc = () => setDoMakeArc(!doMakeArc);
+  const handleTurtleColor = () => setTurtleColor(getRandomColor());
+
   useEffect(() => {
     window.addEventListener('resize', props.updateDimensions);
     return () => window.removeEventListener('resize', props.updateDimensions);
   }, []);
+
+  useEffect(() => {
+    setId(props.id);
+  }, [props]);
   return (
     <>
       <div id="artboard-wrapper">
         <h4>Artboard {`(${props.dimensions[0]} × ${props.dimensions[1]})`}</h4>
-        <ArtboardSketch index={props.id} />
-        <ArtboardTurtle index={props.id + 1} />
+        {
+          <button style={{ position: 'absolute', left: 215, zIndex: 1200000 }} onClick={handleMove}>
+            Move
+          </button>
+        }
+        {
+          <button
+            style={{ position: 'absolute', left: 135, zIndex: 1200000 }}
+            onClick={handleRotate}
+          >
+            Rotate
+          </button>
+        }
+        {
+          <button
+            style={{ position: 'absolute', left: 35, zIndex: 1200000 }}
+            onClick={handleTurtleColor}
+          >
+            Change Color
+          </button>
+        }
+        <ArtboardSketch
+          sketch={boardSketch}
+          index={id}
+          doMove={doMove}
+          rotation={doRotate}
+          sleepTime={100}
+        />
+        <ArtboardTurtleSketch sketch={turtleSketch} index={id + 1} color={turtleColor} />
       </div>
     </>
   );

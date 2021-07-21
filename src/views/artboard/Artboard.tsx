013ec0ee76c26a54a0b79a404dccb3a9eca98fd4
b18050 <abchandan11@gmail.com>
2021-07-21 19:35:37 +0530

    feat: added button for handleArc

--- src/views/artboard/Artboard.tsx
+++ src/views/artboard/Artboard.tsx
@@ -17,9 +17,9 @@ import { ArtboardTurtleSketch, turtleSketch } from './ArtboardTurtle';
  */
 export default function (props: IArtboardProps): JSX.Element {
   const [id, setId] = useState<number>(0);
-  const [doMove, setDoMove] = useState<boolean>();
-  const [doRotate, setDoRotation] = useState<boolean>();
-  const [doMakeArc, setDoMakeArc] = useState<boolean>();
+  const [doMove, setDoMove] = useState<boolean>(false);
+  const [doRotate, setDoRotation] = useState<boolean>(false);
+  const [doMakeArc, setDoMakeArc] = useState<boolean>(false);
   const [turtleColor, setTurtleColor] = useState<string>();
   const colors = ['red', 'magenta', 'pink', 'orange', 'green', 'blue'];
 
@@ -29,6 +29,7 @@ export default function (props: IArtboardProps): JSX.Element {
   const handleMove = () => setDoMove(!doMove);
   const handleRotate = () => setDoRotation(!doRotate);
   const handleArc = () => setDoMakeArc(!doMakeArc);
+
   const handleTurtleColor = () => setTurtleColor(getRandomColor());
 
   useEffect(() => {
@@ -43,33 +44,50 @@ export default function (props: IArtboardProps): JSX.Element {
     <>
       <div id="artboard-wrapper">
         <h4>Artboard {`(${props.dimensions[0]} × ${props.dimensions[1]})`}</h4>
-        {
-          <button style={{ position: 'absolute', left: 215, zIndex: 1200000 }} onClick={handleMove}>
-            Move
-          </button>
-        }
-        {
-          <button
-            style={{ position: 'absolute', left: 135, zIndex: 1200000 }}
-            onClick={handleRotate}
-          >
-            Rotate
-          </button>
-        }
-        {
-          <button
-            style={{ position: 'absolute', left: 35, zIndex: 1200000 }}
-            onClick={handleTurtleColor}
-          >
-            Change Color
-          </button>
-        }
+        <div>
+          {
+            <button
+              style={{ position: 'absolute', left: 265, zIndex: 1200000 }}
+              onClick={handleMove}
+            >
+              Move
+            </button>
+          }
+          {
+            <button
+              style={{ position: 'absolute', left: 205, zIndex: 1200000 }}
+              onClick={handleRotate}
+            >
+              Rotate
+            </button>
+          }
+          {
+            <button
+              style={{ position: 'absolute', left: 135, zIndex: 1200000 }}
+              onClick={handleArc}
+            >
+              Make Arc
+            </button>
+          }
+          {
+            <button
+              style={{ position: 'absolute', left: 35, zIndex: 1200000 }}
+              onClick={handleTurtleColor}
+            >
+              Change Color
+            </button>
+          }
+        </div>
         <ArtboardSketch
           sketch={boardSketch}
           index={id}
           doMove={doMove}
+          handleMove={handleMove}
           rotation={doRotate}
-          sleepTime={100}
+          handleRotation={handleRotate}
+          makeArc={doMakeArc}
+          handleArc={handleArc}
+          sleepTime={50}
         />
         <ArtboardTurtleSketch sketch={turtleSketch} index={id + 1} color={turtleColor} />
       </div>

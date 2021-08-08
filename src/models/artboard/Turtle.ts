69217de672a2e8a47684cb6f3e34d21330f2ab58
joykirat18 <joykiratsingh18@gmail.com>
2021-08-08 12:50:39 +0530

    feat: drag functions implemented

--- src/models/artboard/Turtle.ts
+++ src/models/artboard/Turtle.ts
@@ -13,15 +13,25 @@ export default class implements ITurtleModel {
     _turtleX: number;
     _turtleY: number;
     _turtleAngle: number;
+    _width: number;
+    _height: number;
+
+    /** Dragging parameters */
+    _dragging = false; // Is the object being dragged?
+    _rollover = false; // Is the mouse over the ellipse?
+    _offsetX = 0;
+    _offsetY = 0;
 
     constructor(id: number, x: number, y: number, angle: number) {
         this._id = id;
         this._turtleX = x;
         this._turtleY = y;
         this._turtleAngle = angle;
+        this._width = 30;
+        this._height = 60;
     }
     display(sketch: p5): void {
-        sketch.rect(0, 0, 30, 60);
+        sketch.rect(0, 0, this._width, this._height);
     }
 
     move(sketch: p5): void {
@@ -34,6 +44,55 @@ export default class implements ITurtleModel {
         this.move(sketch);
         this.display(sketch);
         sketch.pop();
+    }
+    over(sketch: p5): void {
+        if (
+            sketch.mouseX > this._turtleX &&
+            sketch.mouseX < this._turtleX + this._width &&
+            sketch.mouseY > this._turtleY &&
+            sketch.mouseY < this._turtleY + this._height
+        ) {
+            this._rollover = true;
+        } else {
+            this._rollover = false;
+        }
+    }
+    update(sketch: p5): void {
+        if (this._dragging) {
+            this._turtleX = sketch.mouseX + this._offsetX;
+            this._turtleY = sketch.mouseY + this._offsetY;
+        }
+    }
+    show(sketch: p5): void {
+        sketch.stroke(0);
+        // Different fill based on state
+        if (this._dragging) {
+            sketch.fill(50);
+        } else if (this._rollover) {
+            sketch.fill(100);
+        } else {
+            sketch.fill(175, 200);
+        }
+        this.render(sketch);
+    }
+    pressed(sketch: p5): void {
+        // Did I click on the rectangle?
+        if (
+            sketch.mouseX > this._turtleX &&
+            sketch.mouseX < this._turtleX + this._width &&
+            sketch.mouseY > this._turtleY &&
+            sketch.mouseY < this._turtleY + this._height
+        ) {
+            this._dragging = true;
+            // If so, keep track of relative location of click to corner of rectangle
+            this._offsetX = this._turtleX - sketch.mouseX;
+            this._offsetY = this._turtleY - sketch.mouseY;
+        }
+    }
+
+    released(): void {
+        // Quit dragging
+        this._dragging = false;
     }
     getTurtleX(): number {
         return this._turtleX;

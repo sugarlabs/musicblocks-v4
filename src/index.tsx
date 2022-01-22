5c9412d6f34a6d292d333c2cfb3459b4e25770f5
Anindya Kundu <anindyaak007@gmail.com>
2022-01-22 21:32:57 +0530

    refactor: Move prototype source code to archive folder

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,13 @@
+import reportWebVitals from './reportWebVitals';
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +16,4 @@ ReactDOM.render(
   document.getElementById('root'),
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// import './archive/index';

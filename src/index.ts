c6d96e34e96d0120a952861972c4ee34d93c2177
Anindya Kundu <anindyaak007@gmail.com>
2022-01-26 10:28:42 +0530

    painter: set up painter

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,20 @@ import reportWebVitals from './reportWebVitals';
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 
+// -------------------------------------------------------------------------------------------------
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App';
+import App from './view/App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
+    React.createElement(React.StrictMode, null, App()),
+    document.getElementById('root'),
 );
+
+setTimeout(() => import('./components'));
+
+// -------------------------------------------------------------------------------------------------
 
 // import './archive/index';

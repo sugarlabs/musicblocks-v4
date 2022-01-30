859f24a8733139b1919142cca460ee0d577ef69f
ishubham21 <sg2199203@gmail.com>
2022-01-30 14:25:05 +0530

    develop: move optimization to webpack.prod.js, add more devServer config

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,10 +3,13 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
     mode: 'development',
-    devtool: 'inline-source-map', /** generate inline source map for easy debugging */
+    devtool: 'inline-source-map', /** generate source map for easy debugging */
     devServer: {
         port: 3000,
         open: true,
-        compress: true
+        compress: true,
+        client: {   /** reconnect to client infinitely */
+            reconnect: true,
+        }
     },
 });

44d9b2c59fb70d1b8e0d12087c9350026b87eac1
Anindya Kundu <anindya.k22@outlook.com>
2022-02-09 19:35:09 +0530

    singer: [feat] implement component API
    - adds component API functions mount and setup in singer/index.ts
    - adds a dry singer.ts module

--- src/components/singer/index.ts
+++ src/components/singer/index.ts
@@ -1,1 +1,25 @@
-export {};
+import { setup as setupComponent } from './singer';
+
+// -- public functions -----------------------------------------------------------------------------
+
+/**
+ * Mounts the Singer component.
+ */
+export function mount(): Promise<void> {
+    return new Promise((resolve) => {
+        resolve();
+    });
+}
+
+/**
+ * Initializes the Singer component.
+ */
+export function setup(): Promise<void> {
+    return new Promise((resolve) => {
+        (async () => {
+            await setupComponent();
+
+            resolve();
+        })();
+    });
+}

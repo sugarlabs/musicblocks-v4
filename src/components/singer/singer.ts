f599de6bb0c620949b55b6f9eb58d3b31e53a6d9
Walter Bender <walter@sorcero.com>
2022-02-20 16:01:24 -0500

    singer: [feat] add test-synth command to singer

--- src/components/singer/singer.ts
+++ src/components/singer/singer.ts
@@ -1,3 +1,10 @@
+import * as Tone from 'tone';
+
+// -- private variables ----------------------------------------------------------------------------
+
+/** Default synth **/
+const _defaultSynth = new Tone.Synth().toDestination();
+
 // -- public functions -----------------------------------------------------------------------------
 
 /**
@@ -7,4 +14,25 @@ export function setup(): Promise<void> {
     return new Promise((resolve) => {
         resolve();
     });
+}
+
+// -- public classes -------------------------------------------------------------------------------
+
+import { ElementStatement } from '@sugarlabs/musicblocks-v4-lib';
+
+/**
+ * @class
+ * Defines a `music` statement element that tests the synth.
+ */
+export class ElementTestSynth extends ElementStatement {
+    constructor() {
+        super('test-synth', 'test synth', {});
+    }
+
+    /**
+     * Plays a predefined note.
+     */
+    onVisit(): void {
+        _defaultSynth.triggerAttackRelease("C4", "8n");
+    }
 }

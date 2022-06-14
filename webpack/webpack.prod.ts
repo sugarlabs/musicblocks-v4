f0c7bab6213015c56952bbfcf5f75f0b550554b6
Anik Das <anikdas0811@gmail.com>
2022-06-14 10:19:08 +0530

    webpack.prod: [feat] copy public/ content to build
    * use copy-webpack-plugin to copy from public to build
    
    * ignore index.html in public as html-webpack-plugin uses that as
      template and generates one for us already
    
    Signed-off-by: Anik Das <anikdas0811@gmail.com>

--- webpack/webpack.prod.ts
+++ webpack/webpack.prod.ts
@@ -1,10 +1,28 @@
 import path from 'path';
 import { Configuration } from 'webpack';
+import CopyPlugin from 'copy-webpack-plugin';
 
 const prodConfig: Configuration = {
     mode: 'production',
     devtool: 'source-map',
-    plugins: [],
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                {
+                    // context prevents the entire `public` folder to be copied
+                    // and copies only the contents
+                    context: 'public',
+                    from: path.join(path.resolve(__dirname, '..', 'public'), '**/*'),
+                    globOptions: {
+                        ignore: [
+                            // Ignore `index.html` as html-webpack-plugin generates one for us
+                            '**/*.html',
+                        ],
+                    },
+                },
+            ],
+        }),
+    ],
     output: {
         path: path.resolve(__dirname, '../build'),
         filename: '[name].[contenthash].js',

e990bd4c2736ce9c2202cd19a3b0d63e626120bc
VictoryGod18 <96035378+VictoryGod18@users.noreply.github.com>
2023-01-04 22:01:28 +0530

    Update index.tsx

--- src/view/components/index.tsx
+++ src/view/components/index.tsx
@@ -1,30 +1,45 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import Toolbar from './toolbar';
-import Splash from './splash';
-
+import Splash from './Splash';
 import './index.scss';
 
-export default function App(): JSX.Element {
+
+export function mountSplash() {
+  ReactDOM.render(
+    <Splash progress={0} />,
+    document.getElementById('root'),
+  );
+}
+
+export function updateSplash(componentCount : number, componentLoadedCount : number) {
+  const progress = (componentLoadedCount / componentCount) * 100;
+  ReactDOM.render(
+    <Splash progress={progress} />,
+    document.getElementById('root'),
+  );
+}
+
+export function unmountSplash() {
+  const rootElement = document.getElementById('root');
+  if (rootElement) {
+    ReactDOM.unmountComponentAtNode(rootElement);
+  }
+}
+
+function App(): JSX.Element {
   return (
     <>
-      <Splash />
       <Toolbar />
       <div id="workspace"></div>
     </>
   );
 }
 
-// -------------------------------------------------------------------------------------------------
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-
 ReactDOM.render(
   <React.StrictMode>
+    
     <App></App>
   </React.StrictMode>,
   document.getElementById('root'),
 );
-
-// -------------------------------------------------------------------------------------------------
-
-

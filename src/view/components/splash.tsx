361ff00b9d02284fdc3058c3f8d77aa0864e5f86
VictoryGod18 <96035378+VictoryGod18@users.noreply.github.com>
2023-01-05 14:43:50 +0530

    Update splash.tsx

--- src/view/components/splash.tsx
+++ src/view/components/splash.tsx
@@ -1,13 +1,66 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import styled from 'styled-components';
+import {App} from './index';
 
-const Splash: React.FC = () => {
+// Define the Container constant inside the Splash component
+const Container = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: #fff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 1000;
+`;
+
+const Logo = styled.img`
+  width: 200px;
+  height: auto;
+`;
+
+const ProgressBar = styled.div`
+  width: 80%;
+  height: 20px;
+  background-color: #ddd;
+  border-radius: 10px;
+  margin: 20px 0;
+`;
+
+const Progress = styled.div`
+  width: 50%;
+  height: 100%;
+  background-color: #333;
+  border-radius: 10px;
+`;
+
+interface Props {
+  progress: number;
+}
+
+export function unmountSplashAndRenderApp() {
+  const rootElement = document.getElementById('root');
+  if (rootElement) {
+    //ReactDOM.unmountComponentAtNode(rootElement);
+    ReactDOM.render(<App />, document.getElementById('root'));
+  }
+  
+}
+
+const Splash: React.FC<Props> = ({ progress }) => {
+   
+
   return (
-    <div className="splash">
-      <div className="splash-content">
-        <h1>Welcome to Music Blocks (v4)</h1>
-        <p>This is a temporary splash screen.</p>
-      </div>
-    </div>
+    // Add an id attribute to the container element
+    <Container id="splash">
+      <Logo src="/logo.png" alt="Logo" />
+      <ProgressBar>
+        <Progress style={{ width: `${progress}%` }} />
+      </ProgressBar>
+    </Container>
   );
 };
 

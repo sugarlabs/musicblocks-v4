52d17573be892866671ec345feafeb92f4860df0
Disha Talreja <dishatalreja1202@gmail.com>
2023-01-13 22:22:36 +0530

    feat(assets): add all svg files to assets/image/icon folder

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -7,6 +7,48 @@ const manifest: {
      * please maintain alphabetical order for readability
      */
 
+    'image.icon.build': {
+        path: './image/icon/build.svg',
+    },
+    'image.icon.close': {
+        path: './image/icon/close.svg',
+    },
+    'image.icon.code': {
+        path: './image/icon/code.svg',
+    },
+    'image.icon.exportDrawing': {
+        path: './image/icon/exportDrawing.svg',
+    },
+    'image.icon.help': {
+        path: './image/icon/help.svg',
+    },
+    'image.icon.mouse': {
+        path: './image/icon/mouse.svg',
+    },
+    'image.icon.pin': {
+        path: './image/icon/pin.svg',
+    },
+    'image.icon.reset': {
+        path: './image/icon/reset.svg',
+    },
+    'image.icon.run': {
+        path: './image/icon/run.svg',
+    },
+    'image.icon.saveProjectHTML': {
+        path: './image/icon/saveProjectHTML.svg',
+    },
+    'image.icon.startRecording': {
+        path: './image/icon/startRecording.svg',
+    },
+    'image.icon.stop': {
+        path: './image/icon/stop.svg',
+    },
+    'image.icon.stopRecording': {
+        path: './image/icon/stopRecording.svg',
+    },
+    'image.icon.unpin': {
+        path: './image/icon/unpin.svg',
+    },
     'image.logo': {
         path: './image/logo.png',
     },

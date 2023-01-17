b0e8e83efe5ba46a034f4fc741391398cf4a5d75
Anindya Kundu <anindya.k22@outlook.com>
2023-01-17 07:50:16 +0530

    feat(core): [config] add separate lifecycle functions
    - importComponents
    - mountComponents
    - setupComponents
    - registerElements

--- src/core/config/index.ts
+++ src/core/config/index.ts
@@ -1,17 +1,178 @@
-import type { IComponent } from '@/@types/components';
+import type { IComponent, TComponentId } from '@/@types/components';
+import type { IElementSpecification } from '@sugarlabs/musicblocks-v4-lib';
+
+import { default as componentMap } from '@/components';
+
+import {
+    registerElementSpecificationEntries,
+    librarySpecification,
+} from '@sugarlabs/musicblocks-v4-lib';
 
 // -- private variables ----------------------------------------------------------------------------
 
-/** Object mapping mounted component names to the  */
-const _components: { [id: string]: IComponent } = {};
+/** Stores key-value pairs of component identifier and component module. */
+const _components: Partial<Record<TComponentId, IComponent>> = {};
+
+// -- private functions ----------------------------------------------------------------------------
+
+/**
+ * Imports a component module.
+ * @param componentId component identifier
+ * @param path path to the module relative to `src/components`
+ * @returns a Promise to the component's module
+ */
+async function _importComponent(componentId: TComponentId, path: string): Promise<IComponent> {
+    _components[componentId] = await import(`../../components/${path}/index.ts`);
+    return _components[componentId]!;
+}
+
+/**
+ * Mounts a component module.
+ * @param componentId component identifier
+ */
+async function _mountComponent(componentId: TComponentId): Promise<void> {
+    const component = _components[componentId]!;
+    await component.mount(component.definition.flags);
+}
+
+/**
+ * Initializes a component module.
+ * @param componentId component identifier
+ */
+async function _setupComponent(componentId: TComponentId): Promise<void> {
+    const component = _components[componentId]!;
+    await component.setup();
+}
 
 // -- public functions -----------------------------------------------------------------------------
 
 /**
  * Returns a component module by it's identifier.
- * @param id identifier of the component
+ * @param componentId component identifier
  * @returns component module if valid identifier else `null`
  */
-export function getComponent(id: string): IComponent | null {
-    return id in _components ? _components[id] : null;
+export function getComponent(componentId: TComponentId): IComponent | null {
+    return componentId in _components ? _components[componentId]! : null;
 }
+
+/**
+ * Imports a list of component modules asynchronously.
+ * @param componentIds list of component identifiers
+ * @param callback callback function to call after succesful import of each component's module
+ * @returns a Promise to the component module map
+ */
+export async function importComponents(
+    componentIds: TComponentId[],
+    callback: (componentId: TComponentId) => unknown,
+): Promise<Partial<Record<TComponentId, IComponent>>> {
+    Object.fromEntries(
+        await Promise.all(
+            componentIds
+                .map((id) => [id, componentMap[id].path] as [TComponentId, string])
+                .map(([id, path]) =>
+                    _importComponent(id, path).then((component) => {
+                        callback(id);
+                        return [id, component];
+                    }),
+                ),
+        ),
+    );
+
+    return _components;
+}
+
+/**
+ * Mounts a list of component modules synchronously.
+ * @param componentIds list of component identifiers
+ * @param callback callback function to call after succesful mounting of each component
+ */
+export async function mountComponents(
+    componentIds: TComponentId[],
+    callback: (componentId: TComponentId) => unknown,
+): Promise<void> {
+    return new Promise((resolve) => {
+        const iterator = componentIds.entries();
+
+        async function _mountHelper(
+            iteratorResult: IteratorResult<[number, TComponentId], unknown>,
+        ): Promise<void> {
+            if (iteratorResult.done) return;
+
+            const [_, componentId] = iteratorResult.value;
+            await _mountComponent(componentId);
+            callback(componentId);
+
+            return _mountHelper(iterator.next());
+        }
+
+        _mountHelper(iterator.next()).then(() => requestAnimationFrame(() => resolve()));
+    });
+}
+
+/**
+ * Initializes a list of component modules synchronously.
+ * @param componentIds list of component identifiers
+ * @param callback callback function to call after succesful initialization of each component
+ */
+export async function setupComponents(
+    componentIds: TComponentId[],
+    callback: (componentId: TComponentId) => unknown,
+): Promise<void> {
+    return new Promise((resolve) => {
+        const iterator = componentIds.entries();
+
+        async function _setupHelper(
+            iteratorResult: IteratorResult<[number, TComponentId], unknown>,
+        ): Promise<void> {
+            if (iteratorResult.done) return;
+
+            const [_, componentId] = iteratorResult.value;
+            await _setupComponent(componentId);
+            callback(componentId);
+
+            return _setupHelper(iterator.next());
+        }
+
+        _setupHelper(iterator.next()).then(() => requestAnimationFrame(() => resolve()));
+    });
+}
+
+/**
+ * Registers syntax elements in the programming engine.
+ * @param entries list of objects of component identifiers and corresponding syntax element filters
+ */
+export function registerElements(
+    entries: {
+        /** Component identifier. */
+        id: TComponentId;
+        /** List of syntax elements to register, or register all if `true`. */
+        filter: string[] | true | undefined;
+    }[],
+) {
+    registerElementSpecificationEntries(librarySpecification);
+
+    entries
+        .filter(({ filter }) => filter !== undefined)
+        .map(({ id, filter }) => {
+            if (filter === true) {
+                return { id, specification: _components[id]!.definition.elements };
+            }
+
+            return {
+                id,
+                specification: Object.fromEntries(
+                    Object.entries(_components[id]!.definition.elements!).filter(([elementName]) =>
+                        filter?.includes(elementName),
+                    ),
+                ),
+            };
+        })
+        .map(
+            ({ specification }) =>
+                specification as { [elementName: string]: IElementSpecification } | undefined,
+        )
+        .filter((specification) => specification !== undefined)
+        .forEach((specification) => registerElementSpecificationEntries(specification!));
+}
+
+export { serializeComponentDependencies } from './utils';

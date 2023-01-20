b0f026648d01d49c9b0189de3ac65cf84085616d
Anindya Kundu <anindyaak007@gmail.com>
2023-01-20 15:07:37 +0530

    refactor(menu): remove unused function

--- src/@types/components/menu.d.ts
+++ src/@types/components/menu.d.ts
@@ -1,29 +1,4 @@
-import type { IComponent } from '.';
 import type { TAsset } from '../core/assets';
-
-/**
- * Interface that represents the Menu component's API.
- */
-export interface IComponentMenu extends IComponent {
-    /**
-     * Mounts a callback associated with `run`.
-     * @param name name of the hook
-     * @param callback callback function to associate with the hook
-     */
-    mountHook(name: 'run', callback: CallableFunction): void;
-    /**
-     * Mounts a callback associated with `reset`.
-     * @param name name of the hook
-     * @param callback callback function to associate with the hook
-     */
-    mountHook(name: 'reset', callback: CallableFunction): void;
-    /**
-     * Mounts a callback associated with a special hook name.
-     * @param name name of the hook
-     * @param callback callback function to associate with the hook
-     */
-    mountHook(name: string, callback: CallableFunction): void;
-}
 
 /** Type definition for feature flag toggles for the Menu component. */
 export type TFeatureFlagMenu = {

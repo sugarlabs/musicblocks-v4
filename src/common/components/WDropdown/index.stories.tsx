b41acd1f954d6d12836c7a95e942780e72ca80f2
Tushar98644 <tusharbanik@Tushars-MacBook-Air.local>
2023-01-21 02:04:29 +0530

    added dropdown

---
+++ src/common/components/WDropdown/index.stories.tsx
@@ -0,0 +1,14 @@
+import type { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { linkTo } from '@storybook/addon-links';
+import { default as WDropdown } from '.';
+
+// import Blog_card from "../PostBlog/blog_card";
+export default {
+    title: "Dropdown",
+    component: WDropdown,
+}
+
+export const Card = () => <WDropdown/>
+
+

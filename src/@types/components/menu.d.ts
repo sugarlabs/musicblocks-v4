f3249be9c7fd0e636132792a828395c97c881347
Anindya Kundu <anindyaak007@gmail.com>
2023-01-20 15:49:14 +0530

    refactor(menu): separate react specific behaviour

--- src/@types/components/menu.d.ts
+++ src/@types/components/menu.d.ts
@@ -9,23 +9,27 @@ export type TFeatureFlagMenu = {
     saveProject: boolean;
 };
 
+/** Type definition for the i18n identifiers defined by the Menu component. */
+export type TI18nMenu = 'menu.run' | 'menu.stop' | 'menu.reset';
+
+/** Type definition for the asset identifiers defined by the Menu component. */
+export type TAssetIdentifierMenu =
+    | 'image.icon.run'
+    | 'image.icon.stop'
+    | 'image.icon.reset'
+    | 'image.icon.saveProjectHTML'
+    | 'image.icon.exportDrawing'
+    | 'image.icon.startRecording'
+    | 'image.icon.stopRecording';
+
 /** Type definition for the items injected into the Menu component after load. */
 export type TInjectedMenu = {
     flags: Record<keyof TFeatureFlagMenu, boolean>;
-    i18n: Record<'menu.run' | 'menu.stop' | 'menu.reset', string>;
-    assets: Record<
-        | 'image.icon.run'
-        | 'image.icon.stop'
-        | 'image.icon.reset'
-        | 'image.icon.saveProjectHTML'
-        | 'image.icon.exportDrawing'
-        | 'image.icon.startRecording'
-        | 'image.icon.stopRecording',
-        TAsset
-    >;
+    i18n: Record<TI18nMenu, string>;
+    assets: Record<TAssetIdentifierMenu, TAsset>;
 };
 
-/** Type definition for the event emitted by the Menu component. */
+/** Type definition for the events emitted by the Menu component. */
 export type TEventMenu =
     | 'menu.run'
     | 'menu.stop'
@@ -36,3 +40,10 @@ export type TEventMenu =
     | 'menu.exportDrawing'
     | 'menu.loadProject'
     | 'menu.saveProject';
+
+/** Type definition for the props to the Menu React component. */
+export type TPropsMenu = {
+    injected: TInjectedMenu;
+    states: Record<'running', boolean>;
+    handlers: Partial<Record<'run' | 'stop' | 'reset', CallableFunction>>;
+};
--- src/components/menu/view/index.ts
+++ src/components/menu/view/index.ts
@@ -1,108 +1,46 @@
+import type { TPropsMenu } from '@/@types/components/menu';
+
 import { createItem } from '@/core/view';
 
-import { setup as setupComponent } from './components';
-
-// import './index.scss';
+import { renderComponent } from './components';
+import { injected } from '..';
 
 // -- private variables ----------------------------------------------------------------------------
 
-let _btnUploadFileInLocalStorage: HTMLInputElement;
-let _btnStartRecording: HTMLButtonElement;
-let _btnStopRecording: HTMLButtonElement;
-let _btnExportDrawing: HTMLButtonElement;
-let _btnLoadProject: HTMLInputElement;
-let _btnSaveProject: HTMLButtonElement;
-let _btnRun: HTMLButtonElement;
-let _btnStop: HTMLButtonElement;
-let _btnReset: HTMLButtonElement;
+let _props: TPropsMenu;
+let _container: HTMLElement;
 
 // -- public functions -----------------------------------------------------------------------------
 
 /**
  * Sets up the DOM.
  */
-export function setup(): Promise<void> {
-    return new Promise((resolve) => {
-        const menu = createItem({
-            location: 'toolbar',
-            type: 'container',
-            position: 'cluster-b',
-        });
-        menu.id = 'menu';
-
-        setupComponent(menu, {
-            labels: {
-                uploadFileInLocalStorage: 'Upload a file',
-                startRecording: 'Start animation Recording',
-                stopRecording: 'Stop animation Recording',
-                exportDrawing: 'Save mouse artwork as PNG',
-                loadProject: 'Load Project',
-                saveProject: 'Save project as HTML',
-            },
-        }).then(
-            ({
-                btnUploadFileInLocalStorage,
-                btnStartRecording,
-                btnStopRecording,
-                btnExportDrawing,
-                btnLoadProject,
-                btnSaveProject,
-                btnRun,
-                btnReset,
-                btnStop,
-            }) => {
-                [
-                    _btnUploadFileInLocalStorage,
-                    _btnStartRecording,
-                    _btnStopRecording,
-                    _btnExportDrawing,
-                    _btnLoadProject,
-                    _btnSaveProject,
-                    _btnRun,
-                    _btnStop,
-                    _btnReset,
-                ] = [
-                    btnUploadFileInLocalStorage,
-                    btnStartRecording,
-                    btnStopRecording,
-                    btnExportDrawing,
-                    btnLoadProject,
-                    btnSaveProject,
-                    btnRun,
-                    btnStop,
-                    btnReset,
-                ];
-                resolve();
-            },
-        );
+export async function mount(): Promise<void> {
+    _container = createItem({
+        location: 'toolbar',
+        type: 'container',
+        position: 'cluster-b',
     });
-}
+    _container.id = 'menu';
 
-/**
- * @returns DOM `uploadFileInLocalStorage` `startRecording` `stopRecording` `exportDrawing`,`loadProject`,`saveProject`,`run`, `stop`, and `reset` buttons
- */
-export function getButtons(): {
-    uploadFileInLocalStorage: HTMLInputElement;
-    startRecording: HTMLButtonElement;
-    stopRecording: HTMLButtonElement;
-    exportDrawing: HTMLButtonElement;
-    loadProject: HTMLInputElement;
-    saveProject: HTMLButtonElement;
-    run: HTMLButtonElement;
-    stop: HTMLButtonElement;
-    reset: HTMLButtonElement;
-} {
-    return {
-        uploadFileInLocalStorage: _btnUploadFileInLocalStorage,
-        startRecording: _btnStartRecording,
-        stopRecording: _btnStopRecording,
-        exportDrawing: _btnExportDrawing,
-        loadProject: _btnLoadProject,
-        saveProject: _btnSaveProject,
-        run: _btnRun,
-        stop: _btnStop,
-        reset: _btnReset,
+    _props = {
+        injected,
+        states: { running: false },
+        handlers: {},
     };
+
+    await renderComponent(_container, { ..._props });
 }
 
-export { updateState } from './components';
+export async function updateState(state: keyof TPropsMenu['states'], value: boolean) {
+    _props.states[state] = value;
+    await renderComponent(_container, { ..._props });
+}
+
+export async function updateHandler(
+    handler: keyof TPropsMenu['handlers'],
+    callback: CallableFunction,
+) {
+    _props.handlers[handler] = callback;
+    await renderComponent(_container, { ..._props });
+}

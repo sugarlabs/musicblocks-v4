00751c648411c868559a9b82bea63abc2dd6bb38
Anindya Kundu <anindyaak007@gmail.com>
2023-01-24 13:02:54 +0530

    chore(tools): remove unused scripts

--- tools/scripts/visualize.ts
+++
@@ -1,7 +0,0 @@
-import open from 'open';
-import preview from './preview';
-
-export default async function (root: string, config: string, port: number) {
-    await preview(root, config, port);
-    open(`http://localhost:${port}/stats.html`);
-}

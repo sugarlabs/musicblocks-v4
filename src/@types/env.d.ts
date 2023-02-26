4aea26ddf632c2d4a6a78b7ab56f6a8b086ec9e1
Anindya Kundu <anindyaak007@gmail.com>
2023-02-26 12:31:52 +0530

    fix(app): [splash, dev] update progress using sizes only in prod

--- src/@types/env.d.ts
+++ src/@types/env.d.ts
@@ -9,13 +9,3 @@ declare module '*.svg' {
 declare module '*.jsonc';
 
 declare module '*.wasm';
-
-declare module 'virtual:stats' {
-    const stats:
-        | {
-              i18n: Record<string, number>;
-              assets: Record<string, number>;
-              modules: Record<string, number>;
-          }
-        | undefined;
-}

93c05a6b13b015e756870f829d9d637ee42e7172
Anindya Kundu <anindyaak007@gmail.com>
2023-06-09 22:55:38 +0530

    feat(assets): migrate to workspace package

---
+++ packages/assets/src/loaders.ts
@@ -0,0 +1,15 @@
+import { TAsset, TAssetType } from '#/@types/assets';
+
+/**
+ * Stores the loader map.
+ * @description A loader transforms the asset data from `base64` if required.
+ */
+const loaders: Partial<Record<TAssetType, (asset: TAsset) => Promise<TAsset>>> = {
+    'image/svg+xml': async (asset: TAsset) => {
+        return fetch(asset.data)
+            .then((res) => res.text())
+            .then((res) => ({ ...asset, data: res }));
+    },
+};
+
+export default loaders;

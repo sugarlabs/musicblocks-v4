3341299c4a991d9a06af1bf5c527a2ee46ecf249
Anindya Kundu <anindyaak007@gmail.com>
2023-06-19 21:05:48 +0530

    feat(view): migrate to workspace package

---
+++ packages/view/index.ts
@@ -0,0 +1,3 @@
+export { createItem } from './src';
+export { initView, setView, mountViewOverlay, unmountViewOverlay } from './src/components';
+export { setToolbarExtended, unsetToolbarExtended } from './src/components/toolbar';

61485a01ed11e6af89226954a3f87fa5fa62f188
Anindya Kundu <anindyaak007@gmail.com>
2023-06-19 22:20:54 +0530

    feat(editor): migrate to workspace package

---
+++ modules/editor/src/index.ts
@@ -0,0 +1,154 @@
+import type { TInjectedEditor } from '#/@types/components/editor';
+
+import { setToolbarExtended, unsetToolbarExtended } from '@sugarlabs/mb4-view';
+import { hearEvent } from '@sugarlabs/mb4-events';
+
+import {
+    getElement,
+    resetStates,
+    setButtonState,
+    setCode,
+    setHelp,
+    setStatus,
+    setup as setupView,
+} from './view';
+import { generateAPI, resetProgram } from './core';
+
+// -- public functions -----------------------------------------------------------------------------
+
+/**
+ * Mounts the UI components.
+ */
+export async function mount(): Promise<void> {
+    return new Promise((resolve) => {
+        (async () => {
+            await setupView();
+            resolve();
+        })();
+    });
+}
+
+/**
+ * Initializes the component.
+ */
+export async function setup(): Promise<void> {
+    hearEvent('menu.reset', () => {
+        setStatus('');
+        resetProgram();
+    });
+
+    setCode(`- clear
+
+# -------------
+# first hexagon
+# -------------
+
+- set-thickness: 4
+- set-color: 5
+- repeat:
+    times:
+      operator-math-plus:
+        operand1: 4
+        operand2: 2
+    scope:
+      - move-forward: 100
+      - turn-right: 60
+
+# --------------
+# second hexagon
+# --------------
+
+- set-color: 9
+- repeat:
+    times: 6
+    scope:
+      - move-forward: 100
+      - turn-left: 60`);
+
+    setCode(`- box-number:
+    name: "a"
+    value: 0
+- box-number:
+    name: "b"
+    value: 1
+- box-number:
+    name: "c"
+    value: 0
+- set-thickness:
+    value:
+      4
+- repeat:
+    times: 10
+    scope:
+      - print:
+          boxidentifier-number: "a"
+      - set-color:
+          boxidentifier-number: "b"
+      - repeat:
+          times: 6
+          scope:
+            - move-forward:
+                operator-math-times:
+                  operand1:
+                    boxidentifier-number: "a"
+                  operand2: 8
+            - turn-left: 90
+      - box-number:
+          name: "c"
+          value:
+            operator-math-plus:
+              operand1:
+                boxidentifier-number: "a"
+              operand2:
+                boxidentifier-number: "b"
+      - box-number:
+          name: "a"
+          value:
+            boxidentifier-number: "b"
+      - box-number:
+          name: "b"
+          value:
+            boxidentifier-number: "c"`);
+
+    setHelp(generateAPI());
+
+    const btn = getElement('button');
+
+    let state: 'initial' | 'float' | 'pinned' = 'initial';
+
+    const setState = (_state: 'initial' | 'float' | 'pinned') => {
+        if (_state === 'initial') {
+            unsetToolbarExtended();
+            resetStates();
+        } else {
+            const toolbarContent = setToolbarExtended('Editor', _state, {
+                pin: () => setState('pinned'),
+                unpin: () => setState('float'),
+            });
+            const editor = getElement('editor');
+            toolbarContent.appendChild(editor);
+        }
+        state = _state;
+    };
+
+    btn.addEventListener('click', () => {
+        if (state === 'initial') {
+            setButtonState('clicked');
+            setState('float');
+        } else {
+            setButtonState('unclicked');
+            setState('initial');
+        }
+    });
+}
+
+// -- public variables -----------------------------------------------------------------------------
+
+export const injected: TInjectedEditor = {
+    // @ts-ignore
+    flags: undefined,
+    // @ts-ignore
+    i18n: undefined,
+    // @ts-ignore
+    assets: undefined,
+};
---
+++ modules/editor/src/view/components/button/index.tsx
@@ -0,0 +1,35 @@
+import { injected } from '../../..';
+
+// -- stylesheet -----------------------------------------------------------------------------------
+
+import './index.scss';
+
+// -- private variables ----------------------------------------------------------------------------
+
+let _container: HTMLElement;
+let _svgCode: string;
+let _svgClose: string;
+
+// -- component definition -------------------------------------------------------------------------
+
+/**
+ * Loads the SVG icons for the editor's toolbar button.
+ * @param container DOM element of the editor's toolbar button
+ */
+export function setup(container: HTMLElement): void {
+  container.id = 'editor-toolbar-btn';
+  _container = container;
+
+  _svgCode = injected.assets['image.icon.code'].data;
+  _svgClose = injected.assets['image.icon.close'].data;
+
+  setButtonImg('code');
+}
+
+/**
+ * Sets the SVG icon for the editor's toolbar button.
+ * @param icon icon name
+ */
+export function setButtonImg(icon: 'code' | 'cross'): void {
+  _container.innerHTML = icon === 'code' ? _svgCode : _svgClose;
+}

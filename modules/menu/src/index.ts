5b371b85d08b528f30a342a50b2c2a7a042b2bb6
Anindya Kundu <anindyaak007@gmail.com>
2023-06-19 22:41:53 +0530

    feat(menu): migrate to workspace package

---
+++ modules/menu/src/index.ts
@@ -0,0 +1,49 @@
+import type { TInjectedMenu } from '#/@types/components/menu';
+
+import { getCrumbs, run } from '@sugarlabs/musicblocks-v4-lib';
+import { emitEvent } from '@sugarlabs/mb4-events';
+
+import { mount as mountView, updateHandler, updateState } from './view';
+
+// -- public functions -----------------------------------------------------------------------------
+
+/**
+ * Mounts the Menu component.
+ */
+export async function mount(): Promise<void> {
+    await mountView();
+}
+
+/**
+ * Initializes the Menu component.
+ */
+export async function setup(): Promise<void> {
+    await updateHandler('run', () => {
+        const crumbs = getCrumbs();
+        if (crumbs.length !== 0) run(getCrumbs()[0].nodeID);
+        updateState('running', true);
+        setTimeout(() => updateState('running', false));
+        emitEvent('menu.run');
+    });
+
+    await updateHandler('stop', () => {
+        updateState('running', false);
+        emitEvent('menu.stop');
+    });
+
+    await updateHandler('reset', () => {
+        updateState('running', false);
+        emitEvent('menu.reset');
+    });
+}
+
+// -- public variables -----------------------------------------------------------------------------
+
+export const injected: TInjectedMenu = {
+    // @ts-ignore
+    flags: undefined,
+    // @ts-ignore
+    i18n: undefined,
+    // @ts-ignore
+    assets: undefined,
+};

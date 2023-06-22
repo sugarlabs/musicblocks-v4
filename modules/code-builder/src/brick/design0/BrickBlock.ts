b51f24a423a4b9d5db78ab70af59546e437e15e6
Anindya Kundu <anindyaak007@gmail.com>
2023-06-22 23:26:44 +0530

    feat(code builder): define classes for each brick type

---
+++ modules/code-builder/src/brick/design0/BrickBlock.ts
@@ -0,0 +1,57 @@
+import type {
+    TBrickArgDataType,
+    TBrickColor,
+    TBrickCoords,
+    TBrickExtent,
+    TBrickKind,
+    TBrickType,
+} from '@/@types/brick';
+
+import { BrickModelBlock } from '../model';
+
+// -------------------------------------------------------------------------------------------------
+
+/**
+ * @class
+ * Final class that defines a block brick.
+ */
+export default class BrickBlock extends BrickModelBlock {
+    constructor(params: {
+        // intrinsic
+        uuid: string;
+        name: string;
+        kind: TBrickKind;
+        type: TBrickType;
+        label: string;
+        glyph: string;
+        args: Record<
+            string,
+            {
+                label: string;
+                dataType: TBrickArgDataType;
+                meta: unknown;
+            }
+        >;
+        // style
+        colorBg: TBrickColor;
+        colorFg: TBrickColor;
+        outline: TBrickColor;
+        scale: number;
+        connectAbove: boolean;
+        connectBelow: boolean;
+    }) {
+        super(params);
+    }
+
+    public get extent(): TBrickExtent {
+        return { width: 0, height: 0 };
+    }
+
+    public get argsCoords(): Record<string, TBrickCoords> {
+        return {};
+    }
+
+    public get SVG(): string {
+        return '';
+    }
+}
---
+++ modules/code-builder/src/brick/design0/BrickExpression.ts
@@ -0,0 +1,56 @@
+import type {
+    TBrickArgDataType,
+    TBrickColor,
+    TBrickCoords,
+    TBrickExtent,
+    TBrickKind,
+    TBrickType,
+} from '@/@types/brick';
+
+import { BrickModelExpression } from '../model';
+
+// -------------------------------------------------------------------------------------------------
+
+/**
+ * @class
+ * Final class that defines a expression brick.
+ */
+export default class BrickExpression extends BrickModelExpression {
+    constructor(params: {
+        // intrinsic
+        uuid: string;
+        name: string;
+        kind: TBrickKind;
+        type: TBrickType;
+        label: string;
+        glyph: string;
+        dataType: TBrickArgDataType;
+        args: Record<
+            string,
+            {
+                label: string;
+                dataType: TBrickArgDataType;
+                meta: unknown;
+            }
+        >;
+        // style
+        colorBg: TBrickColor;
+        colorFg: TBrickColor;
+        outline: TBrickColor;
+        scale: number;
+    }) {
+        super(params);
+    }
+
+    public get extent(): TBrickExtent {
+        return { width: 0, height: 0 };
+    }
+
+    public get argsCoords(): Record<string, TBrickCoords> {
+        return {};
+    }
+
+    public get SVG(): string {
+        return '';
+    }
+}

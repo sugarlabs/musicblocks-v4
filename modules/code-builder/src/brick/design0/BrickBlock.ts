7e4112a5c879cceccd3b3f0550acfa0537e596f5
Niloy Sikdar <niloysikdar30@gmail.com>
2023-06-23 22:07:08 +0530

    feat(code-builder): add SVGs for the bricks

--- modules/code-builder/src/brick/design0/BrickBlock.ts
+++ modules/code-builder/src/brick/design0/BrickBlock.ts
@@ -42,6 +42,49 @@ export default class BrickBlock extends BrickModelBlock {
     }
 
     public get SVG(): string {
-        return '';
+        const argsLength = Object.keys(this._args).length;
+        const repeatPath = '21 '.repeat(argsLength - 1);
+
+        return `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+                <svg
+                width="114"
+                height="129"
+                version="1.1"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlns:svg="http://www.w3.org/2000/svg"
+                >
+                <defs />
+                <g transform="translate(-643,-379)">
+                    <g transform="translate(643,379)">
+                    <g transform="scale(${this._scale})">
+                        <path
+                        d="m 0.5,8.5 v -4 a 4,4 90 0 1 4,-4 h 4 v 2 h 10 v -2 h 9 40 4 a 4,4 90 0 1 4,4 v 4 h -4 v -3 h -4 v 10 h 4 v -3 h 4 
+                                    v 17 h -4 v -3 h -4 v 10 h 4 v -3 h 4 
+                                    v 17 h -4 v -3 h -4 v 10 h 4 v -3 h 4 
+                                    v 4 a 4,4 90 0 1 -4,4 h -4 -40 -1 v 2 h -8 v -2 h -1 -4 a 5,5 90 0 0 -5,5 v 4 4 
+                                    ${repeatPath}
+                                    4 a 5,5 90 0 0 5,5 h 4 v 2 h 10 v -2 h 8 v 6 6 4 a 4,4 90 0 1 -4,4 h -4 -9 -1 v 2 h -8 v -2 h -1 -4 a 4,4 90 0 1 -4,-4 v -4 z"
+                        style="
+                            fill: ${this._colorBg};
+                            fill-opacity: 1;
+                            stroke: ${this._outline};
+                            stroke-width: 1;
+                            stroke-linecap: round;
+                            stroke-opacity: 1;
+                        "
+                        />
+                        <text
+                        style="font-size: 10px; font-family: sans-serif; text-anchor: start; fill: ${this._colorFg};">
+                        <tspan x="20" y="15">${this._label}</tspan>
+                        </text>
+                        <text
+                        style="font-size: 6.66667px; font-family: sans-serif; text-anchor: end; fill: ${this._colorFg};">
+                        <tspan x="65" y="12">${this._label}</tspan>
+                        </text>
+                    </g>
+                    </g>
+                </g>
+                </svg>
+                `;
     }
 }
--- modules/code-builder/src/brick/design0/BrickStatement.ts
+++ modules/code-builder/src/brick/design0/BrickStatement.ts
@@ -42,6 +42,36 @@ export default class BrickStatement extends BrickModelStatement {
     }
 
     public get SVG(): string {
-        return '';
+        const argsLength = Object.keys(this._args).length;
+        const repeatPath = 'v 17 h -4 v -3 h -4 v 10 h 4 v -3 h 4 '.repeat(argsLength - 1);
+
+        return `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+                <svg
+                width="85.5"
+                height="66"
+                version="1.1"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlns:svg="http://www.w3.org/2000/svg">
+                <defs />
+                <g>
+                    <g transform="scale(${this._scale})">
+                    <path
+                        d="m 0.5,8.5 v -4 a 4,4 90 0 1 4,-4 h 4 v 2 h 10 v -2 h 30 4 a 4,4 90 0 1 4,4 v 4 h -4 v -3 h -4 v 10 h 4 v -3 h 4 
+                        ${repeatPath}
+                        v 4 a 4,4 90 0 1 -4,4 h -4 -30 -1 v 2 h -8 v -2 h -1 -4 a 4,4 90 0 1 -4,-4 z"
+                        style="fill:${this._colorBg};fill-opacity:1;stroke:${this._outline};stroke-width:1;stroke-linecap:round;stroke-opacity:1" />
+                    <text
+                        style="font-size:10px;font-family:sans-serif;text-anchor:end;fill:${this._colorFg}">
+                        <tspan x="46" y="24">${this._label}</tspan></text>
+                    <text
+                        style="font-size:6.66667px;font-family:sans-serif;text-anchor:end;fill:${this._colorFg}">
+                        <tspan x="46" y="12">name</tspan></text>
+                    <text
+                        style="font-size:6.66667px;font-family:sans-serif;text-anchor:end;fill:${this._colorFg}">
+                        <tspan x="46" y="33">octave</tspan></text>
+                    </g>
+                </g>
+                </svg>
+                `;
     }
 }

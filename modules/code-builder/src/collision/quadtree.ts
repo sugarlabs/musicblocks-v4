b00a4515adf7dd73abc4f9e65442b0aad81fc931
ABHINAV JHA <75615092+ABHINAV-JHA-27@users.noreply.github.com>
2023-06-26 00:41:29 +0530

    Added Tree Initialiser

--- modules/code-builder/src/collision/quadtree.ts
+++ modules/code-builder/src/collision/quadtree.ts
@@ -1,7 +1,24 @@
-class Quadtree {
-    constructor() {
-        console.log('Quadtree');
+import Quadtree from 'quadtree-lib';
+
+class QuadTree {
+    private _tree;
+    private _width: number;
+    private _height: number;
+    private _maxElements: number;
+
+    constructor(width: number, height: number, maxElements: number) {
+        this._width = width;
+        this._height = height;
+        this._maxElements = maxElements;
+    }
+
+    public init() {
+        this._tree = new Quadtree({
+            width: this._width,
+            height: this._height,
+            maxElements: this._maxElements,
+        });
     }
 }
 
-export default Quadtree;
+export default QuadTree;

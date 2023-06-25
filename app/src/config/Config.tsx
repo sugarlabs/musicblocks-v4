cf3ec04eeb426da0912112ae2a130ba17b51edba
Anindya Kundu <anindyaak007@gmail.com>
2023-06-25 13:59:58 +0530

    feat!: migrate app to workspace package

---
+++ app/src/config/Config.tsx
@@ -0,0 +1,331 @@
+import type { IAppConfig } from '#/@types/app';
+import type { IComponentDefinitionExtended, TComponentId } from '#/@types/components';
+
+import { getConfigCache, updateConfigCache } from '.';
+import { default as componentManifest } from '../components';
+
+// -- ui items -------------------------------------------------------------------------------------
+
+import { WToggleSwitch } from '@sugarlabs/mb4-components';
+
+import './index.scss';
+
+// -- component definition -------------------------------------------------------------------------
+
+/**
+ * React component definition for the feature configurator component.
+ */
+export default function (props: {
+  /** App configurations. */
+  config: IAppConfig;
+  /** Map of component definitions. */
+  definitions: Partial<Record<TComponentId, IComponentDefinitionExtended>>;
+  /** Callback for when configurations are updated. */
+  handlerUpdate: (config: IAppConfig) => unknown;
+}): JSX.Element {
+  // ---------------------------------------------------------------------------
+
+  function _isActive(componentId: TComponentId): boolean {
+    return props.config.components.findIndex(({ id }) => id === componentId) !== -1;
+  }
+
+  function _getElements(componentId: TComponentId): Record<string, boolean> | null {
+    const componentConfig = props.config.components.find(({ id }) => id === componentId);
+
+    if (componentConfig === undefined) return null;
+
+    if (!('elements' in componentConfig) || componentConfig['elements'] === undefined) return null;
+
+    const componentDefintion = props.definitions[componentId]!;
+    const elements = Object.keys(componentDefintion.elements!);
+
+    return componentConfig.elements === true
+      ? Object.fromEntries(elements.map((elementName) => [elementName, true]))
+      : Object.fromEntries(
+          elements.map((elementName) => [
+            elementName,
+            (componentConfig.elements! as string[]).includes(elementName),
+          ]),
+        );
+  }
+
+  function _getFlags(componentId: TComponentId): Record<string, boolean> | null {
+    const componentConfig = props.config.components.find(({ id }) => id === componentId);
+
+    if (componentConfig === undefined) return null;
+
+    return 'flags' in componentConfig ? componentConfig.flags : null;
+  }
+
+  const modules = Object.fromEntries(
+    Object.entries(componentManifest)
+      .map(
+        ([componentId, { name, desc }]) =>
+          ({
+            id: componentId,
+            name,
+            desc,
+          } as {
+            id: TComponentId;
+            name: string;
+            desc: string;
+          }),
+      )
+      .map(
+        (data) =>
+          ({
+            ...data,
+            active: _isActive(data.id),
+          } as {
+            id: TComponentId;
+            name: string;
+            desc: string;
+            active: boolean;
+          }),
+      )
+      .map(
+        (data) =>
+          ({
+            ...data,
+            elements: _getElements(data.id),
+          } as {
+            id: TComponentId;
+            name: string;
+            desc: string;
+            active: boolean;
+            elements: Record<string, boolean> | null;
+          }),
+      )
+      .map(
+        (data) =>
+          ({
+            ...data,
+            flags: _getFlags(data.id),
+          } as {
+            id: TComponentId;
+            name: string;
+            desc: string;
+            active: boolean;
+            elements: Record<string, boolean> | null;
+            flags: Record<string, boolean> | null;
+          }),
+      )
+      .map((data) => [data.id, data]),
+  );
+
+  function _update(callback: (config: IAppConfig) => IAppConfig) {
+    props.handlerUpdate(callback({ ...props.config }));
+  }
+
+  // ---------------------------------------------------------------------------
+
+  function toggleModule(componentId: TComponentId) {
+    _update((config: IAppConfig) => {
+      const configCache = getConfigCache();
+
+      if (!modules[componentId].active) {
+        const newComponentConfig: {
+          id: TComponentId;
+          elements?: string[];
+          flags?: Record<string, boolean>;
+        } = {
+          id: componentId,
+        };
+
+        const configCacheComponentIndex = configCache.components.findIndex(
+          ({ id }) => id === componentId,
+        );
+
+        if ('elements' in props.definitions[componentId]!) {
+          newComponentConfig['elements'] =
+            configCacheComponentIndex !== -1
+              ? // @ts-ignore
+                configCache.components[configCacheComponentIndex].elements
+              : Object.keys(props.definitions[componentId]!.elements!);
+        }
+
+        if ('flags' in props.definitions[componentId]!) {
+          newComponentConfig['flags'] =
+            configCacheComponentIndex !== -1
+              ? // @ts-ignore
+                configCache.components[configCacheComponentIndex].flags
+              : Object.fromEntries(
+                  Object.keys(props.definitions[componentId]!.flags!).map((flag) => [flag, false]),
+                );
+        }
+
+        // @ts-ignore
+        config = { ...config, components: [...config.components, newComponentConfig] };
+      } /** if (modules[componentId].active) */ else {
+        let configCacheComponentIndex = configCache!.components.findIndex(
+          ({ id }) => id === componentId,
+        );
+
+        const configComponentIndex = config.components.findIndex(({ id }) => id === componentId);
+
+        if (configCacheComponentIndex === -1) {
+          updateConfigCache((configCache) => {
+            const newConfigCache = { ...configCache };
+            // @ts-ignore
+            newConfigCache.components.push({ id: componentId });
+            configCacheComponentIndex = newConfigCache.components!.length - 1;
+            return newConfigCache;
+          });
+        }
+
+        if ('flags' in config.components[configComponentIndex]) {
+          updateConfigCache((configCache) => {
+            const newConfigCache = { ...configCache };
+            // @ts-ignore
+            newConfigCache.components[configCacheComponentIndex].flags =
+              //@ts-ignore
+              config.components[configComponentIndex].flags;
+            return newConfigCache;
+          });
+        }
+
+        if ('elements' in config.components[configComponentIndex]) {
+          updateConfigCache((configCache) => {
+            const newConfigCache = { ...configCache };
+            // @ts-ignore
+            newConfigCache.components[configCacheComponentIndex].elements =
+              //@ts-ignore
+              config.components[configComponentIndex].elements;
+            return newConfigCache;
+          });
+        }
+
+        config.components.splice(configComponentIndex, 1);
+      }
+
+      return { ...config };
+    });
+  }
+
+  function toggleModuleElement(componentId: TComponentId, elementName: string) {
+    _update((config: IAppConfig) => {
+      const componentIndex = config.components.findIndex(({ id }) => id === componentId);
+
+      // @ts-ignore
+      if (config.components[componentIndex].elements === true) {
+        // @ts-ignore
+        config.components[componentIndex].elements = Object.entries(_getElements(componentId))
+          .filter(([_, active]) => active)
+          .map(([elementName]) => elementName);
+      }
+
+      // @ts-ignore
+      if (config.components[componentIndex].elements.includes(elementName)) {
+        // @ts-ignore
+        const elementIndex = (config.components[componentIndex].elements as string[]).findIndex(
+          (_elementName) => _elementName === elementName,
+        );
+        // @ts-ignore
+        config.components[componentIndex].elements.splice(elementIndex, 1);
+      } else {
+        // @ts-ignore
+        config.components[componentIndex].elements.push(elementName);
+      }
+
+      return config;
+    });
+  }
+
+  function toggleModuleFlag(componentId: TComponentId, flag: string) {
+    _update((config: IAppConfig) => {
+      const componentIndex = config.components.findIndex(({ id }) => id === componentId);
+      // @ts-ignore
+      config.components[componentIndex].flags[flag] =
+        // @ts-ignore
+        !config.components[componentIndex].flags[flag];
+
+      return config;
+    });
+  }
+
+  // ---------------------------------------------------------------------------
+
+  return (
+    <div id="config-wrapper">
+      <p id="config-disclaimer">refresh webpage to go to main application page</p>
+
+      <ul id="config-module-list">
+        {Object.entries(modules).map(([id, { name, desc, active, elements, flags }], i) => (
+          <li className="config-module-list-item" key={`config-module-list-item-${i}`}>
+            <div className="config-module-list-item-header">
+              <div className="config-module-list-item-header-left">
+                <h2 className="config-module-list-item-name">{name}</h2>
+                <p className="config-module-list-item-desc">{desc}</p>
+              </div>
+              <div className="config-module-list-item-header-right">
+                <WToggleSwitch
+                  active={active}
+                  handlerClick={() => toggleModule(id as TComponentId)}
+                />
+              </div>
+            </div>
+
+            {((elements && Object.keys(elements).length > 0) ||
+              (flags && Object.keys(flags).length > 0)) && (
+              <div
+                className={`config-module-list-item-body ${
+                  !active ? 'config-module-list-item-body-inactive' : ''
+                }`}
+              >
+                {elements && Object.keys(elements).length > 0 && (
+                  <div className="config-module-list-item-subitems config-module-list-item-elements">
+                    <h3 className="config-module-list-item-subitems-label config-module-list-item-elements-label">
+                      Elements
+                    </h3>
+
+                    <ul className="config-module-list-item-subitem-list config-module-list-item-element-list">
+                      {Object.keys(elements).map((element, i) => (
+                        <li
+                          className="config-module-list-item-subitem config-module-list-item-element"
+                          key={`config-module-list-item-element-${i}`}
+                        >
+                          <p className="config-module-list-item-subitem-name config-module-list-item-element-name">
+                            <code>{element}</code>
+                          </p>
+                          <WToggleSwitch
+                            active={elements[element]}
+                            handlerClick={() => toggleModuleElement(id as TComponentId, element)}
+                          />
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                )}
+
+                {flags && Object.keys(flags).length > 0 && (
+                  <div className="config-module-list-item-subitems config-module-list-item-flags">
+                    <h3 className="config-module-list-item-subitems-label config-module-list-item-flags-label">
+                      Feature Flags
+                    </h3>
+
+                    <ul className="config-module-list-item-subitem-list config-module-list-item-flag-list">
+                      {Object.keys(flags).map((flag, i) => (
+                        <li
+                          className="config-module-list-item-subitem config-module-list-item-flag"
+                          key={`config-module-list-item-flag-${i}`}
+                        >
+                          <p className="config-module-list-item-subitem-name config-module-list-item-flag-name">
+                            <code>{flag}</code>
+                          </p>
+                          <WToggleSwitch
+                            active={flags[flag]}
+                            handlerClick={() => toggleModuleFlag(id as TComponentId, flag)}
+                          />
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                )}
+              </div>
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
---
+++ app/src/index.ts
@@ -0,0 +1,393 @@
+import type { IAppConfig, TAppImportMap } from '#/@types/app';
+import type {
+    IComponent,
+    IComponentDefinition,
+    IComponentDefinitionExtended,
+    TComponentId,
+} from '#/@types/components';
+import type { TAsset } from '#/@types/assets';
+import type { TI18nLang } from '#/@types/i18n';
+
+// -------------------------------------------------------------------------------------------------
+
+const _importMap: TAppImportMap = {
+    lang: {},
+    assets: {},
+    components: {},
+};
+
+// -------------------------------------------------------------------------------------------------
+
+async function _loadConfig(preset: number): Promise<IAppConfig> {
+    return (await import(`./config/preset/preset-${preset}.ts`)).default;
+}
+
+function _updateImportMap(item: 'lang', subitem: TI18nLang): TAppImportMap;
+function _updateImportMap(item: 'assets', subitem: string): TAppImportMap;
+function _updateImportMap(item: 'components', subitem: TComponentId): TAppImportMap;
+function _updateImportMap(item: keyof TAppImportMap, subitem?: string): TAppImportMap {
+    if (item === 'lang') {
+        _importMap.lang[subitem as TI18nLang] = true;
+    } else if (item === 'assets') {
+        const assets = { ..._importMap.assets };
+        assets[subitem as string] = true;
+        _importMap.assets = { ...assets };
+    } else if (item === 'components') {
+        const components = { ..._importMap.components };
+        components[subitem as TComponentId] = true;
+        _importMap.components = { ...components };
+    }
+
+    // if (import.meta.env.DEV) console.log(`${item}${subitem ? ` > ${subitem}` : ''}`);
+
+    return _importMap;
+}
+
+// -------------------------------------------------------------------------------------------------
+
+async function init(config?: IAppConfig) {
+    if (config === undefined) {
+        config = await _loadConfig(import.meta.env.VITE_CONFIG_PRESET);
+    }
+
+    const componentManifest = (await import('./components')).default;
+    const assetManifest = (await import('@sugarlabs/mb4-assets')).default;
+
+    const { importAssets, getAssets } = await import('@sugarlabs/mb4-assets');
+
+    let splashTimeStart: number;
+
+    /*
+     * Initialize the application view and mount the spalsh screen.
+     */
+
+    {
+        const { initView, setView } = await import('@sugarlabs/mb4-view');
+        const { mountSplash, definition, injected } = await import('./splash');
+
+        await importAssets(
+            Object.entries(assetManifest)
+                .filter(([identifier]) => (definition.assets as string[]).includes(identifier))
+                .map(([identifier, manifest]) => ({ identifier, manifest })),
+            () => undefined,
+        );
+
+        // @ts-ignore
+        injected.assets = getAssets(definition.assets);
+
+        // Initialize view toolkit
+        await initView();
+        await mountSplash();
+        splashTimeStart = Date.now();
+        await setView('main');
+    }
+
+    /*
+     * Dynamically import components, strings, and assets asynchronously.
+     */
+
+    {
+        const { updateSplash } = await import('./splash');
+
+        const stats = import.meta.env.PROD
+            ? await fetch('stats.json').then((res) => res.json())
+            : undefined;
+
+        const updateSplashData = (data: TAppImportMap) => {
+            if (import.meta.env.DEV) {
+                const total =
+                    1 + Object.keys(data.assets).length + Object.keys(data.components).length;
+                const items =
+                    (data.lang !== undefined ? 1 : 0) +
+                    Object.values(data.assets).filter((flag) => flag).length +
+                    Object.values(data.components).filter((flag) => flag).length;
+                updateSplash((items / total) * 100);
+            } else {
+                const getAssetsSize = (all = true): number => {
+                    return Object.entries(data.assets)
+                        .filter(([, val]) => all || val)
+                        .map(
+                            ([key]) =>
+                                stats.assets[assetManifest[key].path.split('assets/').slice(-1)[0]],
+                        )
+                        .reduce((a, b) => a + b, 0);
+                };
+
+                const getComponentsSize = (all = true): number => {
+                    return Object.entries(data.components)
+                        .filter(([, val]) => all || val)
+                        .map(([key]) => stats.modules[key])
+                        .reduce((a, b) => a + b, 0);
+                };
+
+                const allAssetsSize = getAssetsSize();
+                const loadedAssetsSize = getAssetsSize(false);
+
+                const allComponentsSize = getComponentsSize();
+                const loadedComponentsSize = getComponentsSize(false);
+
+                const langName = Object.keys(data.lang)[0] as TI18nLang;
+                const allLangSize = stats.i18n[langName];
+                const loadedLangSize = data.lang[langName] ? allLangSize : 0;
+
+                const totalSize = allAssetsSize + allComponentsSize + allLangSize;
+                const loadedSize = loadedAssetsSize + loadedComponentsSize + loadedLangSize;
+
+                updateSplash((loadedSize / totalSize) * 100);
+            }
+        };
+
+        const importItemLang = config.env.lang;
+        /** List to component Ids to import. */
+        const importListComponents = config.components.map(({ id }) => id);
+        /** List to asset Ids to import. */
+        const importListAssets = (() => {
+            try {
+                return Object.entries(componentManifest)
+                    .map(
+                        ([
+                            _,
+                            {
+                                definition: { assets },
+                            },
+                        ]) => assets,
+                    )
+                    .reduce((a, b) => [...new Set([...a, ...b])]);
+            } catch (e) {
+                return [];
+            }
+        })();
+
+        _importMap.lang = Object.fromEntries([[importItemLang, false]]);
+        _importMap.assets = Object.fromEntries(importListAssets.map((assetId) => [assetId, false]));
+        _importMap.components = Object.fromEntries(
+            importListComponents.map((componentId) => [componentId, false]),
+        );
+
+        const { importStrings } = await import('@sugarlabs/mb4-i18n');
+        const { importComponent } = await import('@sugarlabs/mb4-config');
+        const { importAsset } = await import('@sugarlabs/mb4-assets');
+
+        await Promise.all([
+            // import ES module for i18n and load strings
+            importStrings(importItemLang).then(() =>
+                updateSplashData(_updateImportMap('lang', importItemLang)),
+            ),
+            // import ES modules for components
+            ...importListComponents.map(
+                (componentId) =>
+                    new Promise<void>((resolve) => {
+                        importComponent(
+                            componentId,
+                            componentManifest[componentId].importFunc,
+                        ).then(() => {
+                            updateSplashData(_updateImportMap('components', componentId));
+                            resolve();
+                        });
+                    }),
+            ),
+            // import asset files and load data
+            ...importListAssets.map(
+                (assetId) =>
+                    new Promise<void>((resolve) => {
+                        importAsset(assetId, assetManifest[assetId]).then(() => {
+                            updateSplashData(_updateImportMap('assets', assetId));
+                            resolve();
+                        });
+                    }),
+            ),
+        ]);
+    }
+
+    /*
+     * Collect imported components.
+     */
+
+    const { getComponent } = await import('@sugarlabs/mb4-config');
+    const componentIds = config.components.map(({ id }) => id);
+
+    /** Map of component identifier and corresponding component module. */
+    const components: Partial<Record<TComponentId, IComponent>> = Object.fromEntries(
+        componentIds.map((componentId) => [componentId, getComponent(componentId)]),
+    );
+    /** List of 2-tuples of component identifier and component definition. */
+    const componentDefinitionEntries: [TComponentId, IComponentDefinition][] = componentIds.map(
+        (componentId) => [componentId, componentManifest[componentId].definition],
+    );
+
+    /**
+     * Inject items into component modules.
+     */
+
+    {
+        const { getStrings } = await import('@sugarlabs/mb4-i18n');
+
+        // Inject i18n strings.
+        componentDefinitionEntries.forEach(
+            ([id, { strings }]) =>
+                (components[id]!.injected.i18n =
+                    Object.keys(strings).length !== 0
+                        ? getStrings(Object.keys(strings))
+                        : undefined),
+        );
+
+        // Inject asset entries.
+        componentDefinitionEntries.forEach(
+            ([id, { assets }]) =>
+                (components[id]!.injected.assets =
+                    assets !== undefined
+                        ? (getAssets(assets) as { [identifier: string]: TAsset })
+                        : undefined),
+        );
+
+        // Inject feature flags.
+        componentDefinitionEntries.forEach(
+            ([componentId]) =>
+                (components[componentId]!.injected.flags = config!.components.find(
+                    ({ id }) => id === componentId,
+                    // @ts-ignore
+                )?.flags),
+        );
+    }
+
+    /**
+     * Serialized list of component identifiers in which the dependent components take precedence.
+     */
+    let componentsOrdered: TComponentId[];
+
+    /*
+     * Complete the application initialization
+     */
+
+    {
+        const {
+            mountComponents,
+            setupComponents,
+            registerElements,
+            serializeComponentDependencies,
+        } = await import('@sugarlabs/mb4-config');
+
+        // Generate serialized list of component identifiers
+        componentsOrdered = serializeComponentDependencies(
+            componentDefinitionEntries
+                .map<[TComponentId, TComponentId[]]>(([id, { dependencies }]) => [
+                    id,
+                    [...new Set([...dependencies.optional, ...dependencies.required])],
+                ])
+                .map(([id, dependencies]) => ({ id, dependencies })),
+        );
+
+        // Register syntax elements as configured for each component
+        registerElements(
+            componentsOrdered.map((componentId) => {
+                return {
+                    id: componentId,
+                    // @ts-ignore
+                    filter: config.components.find(({ id }) => id === componentId)?.elements,
+                };
+            }),
+        );
+
+        // Mount components in serialized order
+        await mountComponents(componentsOrdered);
+
+        // Initialize components in serialized order
+        await setupComponents(componentsOrdered);
+
+        // Unmount the splash screen.
+        const { unmountSplash } = await import('./splash');
+        const splashTimeEnd = Date.now();
+        const splashTime = splashTimeEnd - splashTimeStart;
+        const splashBuffer = Math.max(import.meta.env.VITE_APP_SPLASH_MIN_DELAY - splashTime, 0);
+        setTimeout(() => unmountSplash(), splashBuffer);
+    }
+
+    if (import.meta.env.PROD) {
+        const { loadServiceWorker } = await import('./utils/misc');
+        loadServiceWorker();
+    }
+}
+
+// =================================================================================================
+
+(async function () {
+    /**
+     * if PRODUCTION mode, proceed initializing with configuration preset.
+     */
+
+    if (import.meta.env.PROD) {
+        await init();
+        return;
+    }
+
+    /**
+     * if DEVELOPMENT mode, and configuration in session storage,
+     * proceed initializing with configuration from session storage.
+     */
+
+    {
+        const config = window.sessionStorage.getItem('appConfig');
+
+        if (config !== null) {
+            await init(JSON.parse(config) as IAppConfig);
+            return;
+        }
+    }
+
+    /**
+     * if DEVELOPMENT mode, and configuration not in session storage,
+     * open configurator page.
+     * @todo currently needs refresh to go to main app page
+     */
+
+    {
+        const config = await _loadConfig(import.meta.env.VITE_CONFIG_PRESET);
+
+        const { initView } = await import('@sugarlabs/mb4-view');
+        await initView();
+
+        window.sessionStorage.setItem('appConfig', JSON.stringify(config));
+
+        const componentManifest = (await import('./components')).default;
+        const { importComponent } = await import('@sugarlabs/mb4-config');
+        const components = Object.fromEntries(
+            await Promise.all(
+                (Object.keys(componentManifest) as TComponentId[]).map((componentId) =>
+                    importComponent(componentId, componentManifest[componentId].importFunc).then(
+                        (component) => [componentId, component],
+                    ),
+                ),
+            ),
+        ) as Record<TComponentId, IComponent>;
+        const componentDefinitions = Object.fromEntries(
+            Object.entries(componentManifest).map<[TComponentId, IComponentDefinitionExtended]>(
+                ([componentId, { definition }]) => [
+                    componentId as TComponentId,
+                    { ...definition, elements: components[componentId as TComponentId].elements },
+                ],
+            ),
+        ) as Record<TComponentId, IComponentDefinition>;
+
+        const { mountConfigPage, updateConfigPage } = await import('./config');
+
+        await mountConfigPage(
+            { ...config },
+            Object.fromEntries(
+                (
+                    Object.entries(componentDefinitions) as [TComponentId, IComponentDefinition][]
+                ).map(([componentId, definition]) => [
+                    componentId,
+                    {
+                        ...definition,
+                        elements: components[componentId]?.elements,
+                    },
+                ]),
+            ),
+            (config: IAppConfig) =>
+                requestAnimationFrame(() => {
+                    window.sessionStorage.setItem('appConfig', JSON.stringify(config));
+                    updateConfigPage(config);
+                }),
+        );
+    }
+})();

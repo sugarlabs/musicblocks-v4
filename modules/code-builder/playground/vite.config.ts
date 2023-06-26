a8248718d335d4a405224a299a02e73541a45653
Anindya Kundu <anindyaak007@gmail.com>
2023-06-26 19:02:38 +0530

    feat: add playground app
    for spinning up react pages for evalutation

---
+++ modules/code-builder/playground/vite.config.ts
@@ -0,0 +1,18 @@
+import path from 'path';
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+    plugins: [
+        //
+        react(),
+    ],
+
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, '..', 'src'),
+        },
+        extensions: ['.tsx', '.ts', '.js', '.scss', '.sass', '.json'],
+    },
+});

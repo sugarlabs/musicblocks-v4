1b1db8aa4ec5741a95331297cef50145fc200674
Niloy Sikdar <niloysikdar30@gmail.com>
2023-06-29 01:44:57 +0530

    fix(storybook): fix NoArgs stories for the Bricks

--- modules/code-builder/src/brick/design0/stories/BrickStatement.stories.ts
+++ modules/code-builder/src/brick/design0/stories/BrickStatement.stories.ts
@@ -12,7 +12,7 @@ export const NoArgs: Story = {
     args: {
         prototype: BrickStatement,
         label: 'Statement',
-        args: ['Label 1'],
+        args: [],
         colorBg: 'yellow',
         colorFg: 'black',
         outline: 'red',

380bcc924fb30c549b2d0a7f163f7de258d08b91
Niloy Sikdar <niloysikdar30@gmail.com>
2023-06-29 01:41:53 +0530

    docs(code-builder): update BrickStatement story

--- modules/code-builder/src/brick/stories/components/BrickStatement.tsx
+++ modules/code-builder/src/brick/stories/components/BrickStatement.tsx
@@ -1,53 +1,10 @@
-import type { IBrickStatement, TBrickArgDataType, TBrickColor } from '@/@types/brick';
+import BrickStatement from '@/brick/design0/components/BrickStatement';
+import type { TBrickStatementProps } from '@/brick/design0/components/BrickStatement';
 
-import Brick from './Brick';
-
-export default function (props: {
-  prototype: new (params: {
-    name: string;
-    label: string;
-    glyph: string;
-    args: Record<
-      string,
-      {
-        label: string;
-        dataType: TBrickArgDataType;
-        meta: unknown;
-      }
-    >;
-    colorBg: TBrickColor;
-    colorFg: TBrickColor;
-    outline: TBrickColor;
-    scale: number;
-    connectAbove: boolean;
-    connectBelow: boolean;
-  }) => IBrickStatement;
-  label: string;
-  args: string[];
-  colorBg: string;
-  colorFg: string;
-  outline: string;
-  scale: number;
-}): JSX.Element {
-  const { prototype, label, args, colorBg, colorFg, outline, scale } = props;
-
-  const instance = new prototype({
-    label,
-    args: Object.fromEntries(
-      args.map<[string, { label: string; dataType: TBrickArgDataType; meta: unknown }]>((name) => [
-        name,
-        { label: name, dataType: 'any', meta: undefined },
-      ]),
-    ),
-    colorBg,
-    colorFg,
-    outline,
-    scale,
-    glyph: '',
-    connectAbove: true,
-    connectBelow: true,
-    name: '',
-  });
-
-  return <Brick svg={instance.SVG} />;
+export default function (props: TBrickStatementProps): JSX.Element {
+  return (
+    <svg version="1.1" xmlns="http://www.w3.org/2000/svg">
+      <BrickStatement {...props} />
+    </svg>
+  );
 }

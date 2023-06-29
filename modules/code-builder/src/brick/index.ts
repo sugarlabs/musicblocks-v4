73c626bcef51abb368f401bb11744a2f84fccd3e
Anindya Kundu <anindyaak007@gmail.com>
2023-06-29 14:56:58 +0530

    feat(code-builder): [brick] export components from design0

--- modules/code-builder/src/brick/index.ts
+++ modules/code-builder/src/brick/index.ts
@@ -1,4 +1,9 @@
-export { default as BrickData } from './design0/BrickData';
-export { default as BrickExpression } from './design0/BrickExpression';
-export { default as BrickStatement } from './design0/BrickStatement';
-export { default as BrickBlock } from './design0/BrickBlock';
+export { default as ModelBrickData } from './design0/BrickData';
+export { default as ModelBrickExpression } from './design0/BrickExpression';
+export { default as ModelBrickStatement } from './design0/BrickStatement';
+export { default as ModelBrickBlock } from './design0/BrickBlock';
+
+export { default as BrickData } from './design0/components/BrickData';
+export { default as BrickExpression } from './design0/components/BrickExpression';
+export { default as BrickStatement } from './design0/components/BrickStatement';
+export { default as BrickBlock } from './design0/components/BrickBlock';

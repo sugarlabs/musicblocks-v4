4032742ab839f199e14fa04ce92bf66bdcee11b2
Anindya Kundu <anindyaak007@gmail.com>
2023-07-02 17:58:02 +0530

    feat(singer): add playground app

---
+++ modules/singer/playground/index.tsx
@@ -0,0 +1,18 @@
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom';
+
+import PageVoice from './pages/Voice';
+
+const router = createBrowserRouter([
+  {
+    path: 'voice',
+    element: <PageVoice />,
+  },
+  {
+    path: '/',
+    element: <Navigate to="voice" />,
+  },
+]);
+
+const root = createRoot(document.getElementById('playground-root') as HTMLElement);
+root.render(<RouterProvider router={router} />);

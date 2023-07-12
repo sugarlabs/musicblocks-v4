49d4c49f62baece58835f2235d819a10e3f6783f
ABHINAV JHA <75615092+ABHINAV-JHA-27@users.noreply.github.com>
2023-07-12 16:40:35 +0530

    (fix): Added positioning based on px instead of percentages

--- modules/code-builder/src/brick/design0/components/BrickExpression.tsx
+++ modules/code-builder/src/brick/design0/components/BrickExpression.tsx
@@ -59,8 +59,8 @@ export default function (props: { instance: IBrickExpression }): JSX.Element {
         {argLabels.map((argLabel, index) => (
           <text
             key={index}
-            x="30%"
-            y={`${index == 0 ? 8 : 5 * index + (index + 1) * 8}%`}
+            x={`${(labelRef.current?.getBBox().width as number) + 25}px`}
+            y={`${index == 0 ? 11 : 9 * index + (index + 1) * 11}px`}
             dominantBaseline="middle"
             style={{
               fontSize: '0.8em',

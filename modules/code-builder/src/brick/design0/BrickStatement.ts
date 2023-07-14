db26e13ce2e69fbc8f6287fe381712d68eedb734
Niloy Sikdar <niloysikdar30@gmail.com>
2023-07-14 17:11:18 +0530

    fix(bricks): change `BrickStatement` model to adapt the new type

--- modules/code-builder/src/brick/design0/BrickStatement.ts
+++ modules/code-builder/src/brick/design0/BrickStatement.ts
@@ -10,6 +10,8 @@ import { generatePath } from './utils/path';
  * Final class that defines a statement brick.
  */
 export default class BrickStatement extends BrickModelStatement {
+    readonly _pathResults: ReturnType<typeof generatePath>;
+
     constructor(params: {
         // intrinsic
         name: string;
@@ -32,32 +34,47 @@ export default class BrickStatement extends BrickModelStatement {
         connectBelow: boolean;
     }) {
         super(params);
-    }
-
-    public get extent(): TBrickExtent {
-        return { width: 0, height: 0 };
-    }
-
-    public get argsCoords(): Record<string, TBrickCoords> {
-        return {};
-    }
-
-    public get SVGpaths(): string[] {
-        const argsLength = Object.keys(this._args).length;
-        let result: string[] = [];
-
-        const path = generatePath({
+        const argsKeys = Object.keys(this._args);
+        this._pathResults = generatePath({
             hasNest: false,
             hasNotchArg: false,
             hasNotchInsTop: true,
             hasNotchInsBot: true,
             scale: this._scale,
             innerLengthX: 100,
-            argHeights: Array.from({ length: argsLength }, () => 17),
-        }).path;
+            argHeights: Array.from({ length: argsKeys.length }, () => 17),
+        });
+    }
 
-        result.push(path);
+    public get SVGpath(): string {
+        return this._pathResults.path;
+    }
+
+    public get bBoxBrick(): { extent: TBrickExtent; coords: TBrickCoords } {
+        return this._pathResults.bBoxBrick;
+    }
+
+    public get bBoxArgs(): Record<string, { extent: TBrickExtent; coords: TBrickCoords }> {
+        const argsKeys = Object.keys(this._args);
+        const result: Record<string, { extent: TBrickExtent; coords: TBrickCoords }> = {};
+
+        argsKeys.forEach((key, index) => {
+            result[key] = { extent: { width: 0, height: 0 }, coords: { x: 0, y: 0 } };
+            const argX = this._pathResults.bBoxArgs.coords[index].x;
+            const argY = this._pathResults.bBoxArgs.coords[index].y;
+
+            result[key].extent = this._pathResults.bBoxArgs.extent;
+            result[key].coords = { x: argX, y: argY };
+        });
 
         return result;
+    }
+
+    public get bBoxNotchInsTop(): { extent: TBrickExtent; coords: TBrickCoords } {
+        return this._pathResults.bBoxNotchInsTop!;
+    }
+
+    public get bBoxNotchInsBot(): { extent: TBrickExtent; coords: TBrickCoords } {
+        return this._pathResults.bBoxNotchInsBot!;
     }
 }

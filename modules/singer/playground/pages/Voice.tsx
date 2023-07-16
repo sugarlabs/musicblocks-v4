6f3b5e5c9ca1c05c49408038758f2a11bf8ce38c
Sheel2007 <sheel_arsenal@icloud.com>
2023-07-16 08:16:42 -0400

    feat(singer) Work in progess synth

--- modules/singer/playground/pages/Voice.tsx
+++ modules/singer/playground/pages/Voice.tsx
@@ -1,6 +1,9 @@
-import { ElementStatement, TData } from '@sugarlabs/musicblocks-v4-lib';
-import { _stateObj, _defaultSynth, _polySynth, noteValueToSeconds, _state } from '@/singer';
+import { Voice } from '@/core/voice';
+import SynthUtils from '@/core/synthUtils';
+import loaders from '@sugarlabs/mb4-assets/src/loaders';
 import * as Tone from 'tone';
+import { importAssets } from '@sugarlabs/mb4-assets';
+import { _state, noteValueToSeconds, _defaultSynth, _polySynth } from '@/singer';
 
 function _getSynth(synthType: String) {
   switch (synthType) {
@@ -21,6 +24,19 @@ async function playSynth(synthType: String) {
   _state.notesPlayed += 4;
 }
 
+async function voice() {
+  var synth = new SynthUtils();
+  // const myVoice = new Voice('myvoice', synth);
+  // myVoice.playNote('c4', 1 / 4, 'piano');
+  synth.trigger(['c4'], 4, 'piano', 3);
+
+  // _state.notesPlayed = 0;
+  // const now = Tone.now();
+  // let offset = noteValueToSeconds(_state.notesPlayed);
+  // synth.trigger(['c4', 'd4'], 4, 'electronic synth', now + offset);
+  // _state.notesPlayed += 4;
+}
+
 export default function (): JSX.Element {
 
     return (
@@ -28,6 +44,7 @@ export default function (): JSX.Element {
         <h1>Voice Component</h1>
         <button onClick={async () => {await playSynth('default');}}>Default Synth</button>
         <button onClick={async () => {await playSynth('polysynth');}}>PolySynth</button>
+        <button onClick={async () => {await voice();}}>Voice Sample Synth</button>
       </div>
     );
 }

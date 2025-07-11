875ce7d80d3caa43e2c04f7b50242e7c99d970ec
Saumya Shahi <saumyashahi05@gmail.com>
2025-07-11 13:41:05 +0530

    feat(Masonry): [playground] add index entry for playground app

--- modules/masonry/playground/index.tsx
+++ modules/masonry/playground/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import '../src/palette/palette.css';
-import App from './pages/workspace/index';
+import App from './App';
 import { RecoilRoot } from 'recoil';
 
 ReactDOM.createRoot(document.getElementById('playground-root')!).render(

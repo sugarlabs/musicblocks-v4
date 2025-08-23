1f1b9b0d1083cbec756754941bca2c948cdd185b
Safwan Sayeed <isafwansayeed@gmail.com>
2025-08-23 13:02:15 +0530

    feat(tests): Export MusicBlocksScheduler and PluginManager with associated types

--- modules/testing/src/index.ts
+++ modules/testing/src/index.ts
@@ -2,3 +2,14 @@
  * Testing module for Music Blocks execution engine
  * Provides plugin system and end-to-end testing capabilities
  */
+
+export { MusicBlocksScheduler } from './music-blocks-scheduler';
+export type { SchedulerStatus } from './music-blocks-scheduler';
+
+export { PluginManager } from './plugins/plugin-manager';
+export type {
+    IPlugin,
+    PluginResult,
+    ExecutionContext,
+    PluginArgs,
+} from './plugins/plugin-interface';

4f9f248c85eae1a9e37fb57e47225a1eb7a7c48c
Anindya Kundu <anindyaak007@gmail.com>
2026-07-09 18:12:23 +0530

    #566 | feat(masonry): create Workspace shell component

--- modules/masonry/src/playground/pages/Workspace.tsx
+++ modules/masonry/src/playground/pages/Workspace.tsx
@@ -1,10 +1,11 @@
-import Workspace from '@/components/Workspace/Workspace';
+import { Workspace } from '@/components/Workspace/Workspace';
+import { mockPaletteConfig } from '@/mocks/palette';
 
 export default function WorkspaceDemo() {
   return (
-    <div className="h-full w-full bg-indigo-100 px-2">
-      <div className="h-full w-full bg-white">
-        <Workspace />
+    <div className="h-full w-full bg-indigo-100 p-2">
+      <div className="h-full w-full rounded-lg bg-white">
+        <Workspace config={{ palette: mockPaletteConfig }} />
       </div>
     </div>
   );

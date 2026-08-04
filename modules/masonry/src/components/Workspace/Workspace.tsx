e68e6d4f1be632a466b1610feeda7be782f74374
Parth Dagia <parth.24bcs10414@sst.scaler.com>
2026-08-04 19:17:28 +0530

    #754 | feat(masonry): add a scale control to the Workspace canvas

--- modules/masonry/src/components/Workspace/Workspace.tsx
+++ modules/masonry/src/components/Workspace/Workspace.tsx
@@ -14,6 +14,7 @@ import { useWorkspaceStore } from '@/stores/workspace';
 import { listNodes } from '@/utils/tower-traversal';
 
 import { DragGhost } from './DragGhost';
+import { ScaleControl } from './ScaleControl';
 
 function TowerLayoutEngine({ root, origin }: { root: TowerNode; origin: Point }) {
   useTowerLayout(root, origin);
@@ -89,6 +90,7 @@ export function Workspace({ config }: WorkspaceViewProps) {
         {allNodes.map((node) => (
           <TowerBrickView key={node.model.id} id={node.model.id} node={node} />
         ))}
+        <ScaleControl />
       </div>
 
       <DragGhost ref={ghostRef} />
